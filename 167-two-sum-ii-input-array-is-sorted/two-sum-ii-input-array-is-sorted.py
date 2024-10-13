2ff9e80730a384ee2f219093c3d0382371728ed0
Dhaval Patel <dhavaljp.95@gmail.com>
2024-10-12 18:34:12 -0700

    Time: 103 ms (68.36%) | Memory: 17.8 MB (54.03%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,13 +1,17 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        hmap = {}
-        nums = numbers
-        for i in range(len(nums)):
-            sum = target - nums[i]
-            print(hmap)
-            if sum not in hmap:
-                hmap[nums[i]] = i
+        i,j = 0, len(numbers)-1
+
+        op = []
+        sum = 0
+    
+        while i < j:
+            sum = numbers[i] + numbers[j]
+            if sum > target:
+                j-=1
+            elif sum < target:
+                i+=1
             else:
-                soln = [hmap[sum]+1, i+1]
-                return soln
-        
+                op.append(i+1)
+                op.append(j+1)
+                return op
