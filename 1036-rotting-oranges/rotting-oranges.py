ea1acd0bb3c75d66529874fe1e8745e588058672
Dhaval Patel <dhavaljp.95@gmail.com>
2024-10-30 16:20:30 -0700

    Time: 7 ms (28.20%) | Memory: 16.9 MB (11.31%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -5,19 +5,24 @@ class Solution:
         queue = collections.deque()
         directions = [[0,1], [0, -1], [1,0], [-1, 0]]
 
-        fresh = 0
-        time = 0
+        fresh_orange = False
 
         for r in range(row_count):
             for c in range(col_count):
                 if grid[r][c] == 1:
-                    fresh += 1
+                    fresh_orange = True
+        
+        if not fresh_orange:
+            return 0
+
+        for r in range(row_count):
+            for c in range(col_count):
                 if grid[r][c] == 2:
                     queue.append((r,c,0))
-
-        while fresh >= 0 and queue:
+        
+        while queue:
             r, c, time = queue.popleft()
-            print("Time", time, queue, fresh)
+            print("Time", time)
             for dr, dc in directions:
                 row = r + dr
                 col = c + dc
@@ -29,7 +34,13 @@ class Solution:
                     grid[row][col] == 1):
 
                     grid[row][col] = 2
+
                     queue.append((row, col, time + 1))
-                    fresh -= 1
         
-        return time if fresh == 0 else -1
+        for r in range(row_count):
+            for c in range(col_count):
+                if grid[r][c] == 1:
+                    return -1
+        
+        return time
+
