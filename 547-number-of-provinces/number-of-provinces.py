1af17806535a1f72eee93d87bceb22bee949d61c
Dhaval Patel <dhavaljp.95@gmail.com>
2024-10-29 17:40:48 -0700

    Time: 7 ms (57.25%) | Memory: 18.2 MB (7.43%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -6,12 +6,12 @@ class Solution:
         def dfs(node):
             visit[node] = 1
             for i in range(len(isConnected)):
-                if isConnected[i][node] == 1 and isConnected[node][i] == 1:
+                if isConnected[i][node] == 1:
                     if visit[i] == 0:
                         dfs(i)
             
 
-        for node in range(len(isConnected[0])):
+        for node in range(len(isConnected)):
             if visit[node] == 0:
                 count +=1
                 dfs(node)
