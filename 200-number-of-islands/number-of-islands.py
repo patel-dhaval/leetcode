d1f88394e39849d361f034333f4080d53efd3804
Dhaval Patel <dhavaljp.95@gmail.com>
2024-10-29 20:51:31 -0700

    Time: 310 ms (11.78%) | Memory: 24.4 MB (19.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -8,27 +8,21 @@ class Solution:
         visited=set()
         islands=0
 
-        def bfs(r,c):
-            q = deque()
+        def dfs(r,c):
             visited.add((r,c))
-            q.append((r,c))
-           
-            while q:
-                row,col = q.popleft()
-                directions= [[1,0],[-1,0],[0,1],[0,-1]]
-            
-                for dr,dc in directions:
-                    r,c = row + dr, col + dc
-                    if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
-                    
-                        q.append((r , c ))
-                        visited.add((r, c ))
+            row, col = r, c
+            directions= [[1,0],[-1,0],[0,1],[0,-1]]
+        
+            for dr,dc in directions:
+                r,c = row + dr, col + dc
+                if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
+                    dfs(r, c)
 
         for r in range(rows):
             for c in range(cols):
             
                 if grid[r][c] == "1" and (r,c) not in visited:
-                    bfs(r,c)
+                    dfs(r,c)
                     islands +=1 
 
         return islands
