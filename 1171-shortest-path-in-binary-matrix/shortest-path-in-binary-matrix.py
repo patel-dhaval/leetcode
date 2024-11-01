fa40ab5471b781d360e624fed1c72501ee12a9fc
Dhaval Patel <dhavaljp.95@gmail.com>
2024-11-01 16:27:16 -0700

    Time: 259 ms (25.37%) | Memory: 18.4 MB (11.26%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -10,24 +10,20 @@ class Solution:
 
         directions = [[0,1],[0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
 
-        queue.append((0, 0))
+        queue.append((0, 0, 1))
         visit.add((0,0))
-        length +=1
 
         while queue:
-            for i in range(len(queue)):
-                row, col = queue.popleft()
-                
-                if row == rows-1 and col == cols - 1:
-                    return length
+            row, col, length = queue.popleft()
+            
+            if row == rows-1 and col == cols - 1:
+                return length
 
-                for dr, dc in directions:
-                    r = row + dr
-                    c = col + dc
-                    if min(r, c) < 0 or r >= rows or c >= cols or (r, c) in visit or grid[r][c] != 0:
-                        continue
-                    queue.append((r,c))
-                    visit.add((r,c))
-            length +=1
-        
+            for dr, dc in directions:
+                r = row + dr
+                c = col + dc
+                if min(r, c) < 0 or r >= rows or c >= cols or (r, c) in visit or grid[r][c] != 0:
+                    continue
+                queue.append((r,c, length +1 ))
+                visit.add((r,c)) 
         return -1
