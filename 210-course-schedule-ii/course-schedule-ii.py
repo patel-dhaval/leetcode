0cea8e2cf7cbb0c7593d6ef33c2c5009a6c6b2c4
Dhaval Patel <dhavaljp.95@gmail.com>
2024-11-01 19:52:18 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (99.31%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -2,11 +2,12 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         indegree = [0] * numCourses
         queue = collections.deque()
-        
+
         adj = [[] for i in range(numCourses)]
-        for nxt, pre in prerequisites:
-            indegree[nxt] += 1
-            adj[pre].append(nxt)
+        for crs, pre in prerequisites:
+            indegree[crs] += 1
+            adj[pre].append(crs)
+
         
         for node in range(numCourses):
             if indegree[node] == 0:
@@ -14,10 +15,10 @@ class Solution:
 
         output = []
         while queue:
-            curr = queue.popleft()
-            output.append(curr)
+            node = queue.popleft()
+            output.append(node)
 
-            for nei in adj[curr]:
+            for nei in adj[node]:
                 indegree[nei] -= 1
 
                 if indegree[nei] == 0:
