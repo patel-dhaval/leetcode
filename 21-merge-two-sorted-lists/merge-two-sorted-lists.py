9710c74cf6cbcb142b56a2c1f768630d6dc73c67
Dhaval Patel <dhavaljp.95@gmail.com>
2025-02-09 18:47:45 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (90.90%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -3,20 +3,27 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        dummy = node = ListNode()
+'''
+1. Start with a dummy node
+2. Compare the values of heads of both lists
+3. Whichever is smaller, dummy node points to that one
+4. Keep going till we reach the end of either of the two lists
+5. Then keep adding the values from the list whichever has any left over values, since the list is sorted, this will work just fine.
+6. Return dummy.next to get the merged sorted lists
 
+'''
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = curr =  ListNode()
         while list1 and list2:
             if list1.val < list2.val:
-                node.next = list1
+                curr.next = list1
                 list1 = list1.next
             else:
-                node.next = list2
+                curr.next = list2
                 list2 = list2.next
-            node = node.next
+            curr = curr.next
 
-        node.next = list1 or list2
+        curr.next = list1 or list2
 
         return dummy.next
-        
