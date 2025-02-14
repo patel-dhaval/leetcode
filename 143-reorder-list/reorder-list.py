6a2b68cd07109f53ecd403f737acea6e639e3036
Dhaval Patel <dhavaljp.95@gmail.com>
2025-02-14 07:18:00 -0500

    Time: 4 ms (37.41%) | Memory: 23.2 MB (92.99%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -1,31 +1,48 @@
+'''
+Approach
+
+1. Find the middle of the list as the plan would be to reverse the second half of the LL and merge the two halves
+2. Use Fast and Slow pointers to find the middle of the list, be careful for both even and odd length of LL
+3. Once the middle of the LL is found, we save the starting node for the second half of the LL
+4. Reverse the second half of the LL
+5. We iterate by moving from the head to the next pointer being the head node of the second half of the LL
+'''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reorderList(self, head: ListNode) -> None:
-        # find middle
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
         slow, fast = head, head.next
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        # reverse second half
-        second = slow.next
-        prev = slow.next = None
-        while second:
-            tmp = second.next
-            second.next = prev
-            prev = second
-            second = tmp
-
-        # merge two halfs
-        first, second = head, prev
-        while second:
-            tmp1, tmp2 = first.next, second.next
-            first.next = second
-            second.next = tmp1
-            first, second = tmp1, tmp2
-
         
+        curr = slow.next
+        slow.next = None
+        prev = None
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        head1 = head
+        head2 = prev
+
+        while head2:
+            temp = head1.next
+            temp2 = head2.next
+            head1.next = head2
+            head2.next = temp
+            head1 = temp
+            head2 = temp2
