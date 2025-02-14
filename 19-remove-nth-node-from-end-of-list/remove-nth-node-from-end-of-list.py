558ceabc07d3a8a2e87d4f34bbe9f6a13057cdf4
Dhaval Patel <dhavaljp.95@gmail.com>
2025-02-14 07:31:52 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.29%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -1,3 +1,12 @@
+'''
+Approach
+
+The solution of this would be a slightly modified version of slow and fast pointers
+We will take two pointers, and move them n places apart, n will be part of the input given
+Once the nodes are n-spaces apart, we move the nodes by 1 spaces till the second node reaches the end of the list
+Once the second node reaches the end, we will have the first node at n-1th spot from the end of the list
+We simply update the pointers and we drop the nth node
+'''
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,18 +14,23 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        
+        if not head.next:
+            return None
+
         dummy = ListNode(0, head)
-        left = dummy
-        right = head
+        first = dummy
+        second = head
 
         while n > 0:
-            right = right.next
-            n -= 1
+            second = second.next
+            n = n-1
+        
+        while second:
+            first = first.next
+            second = second.next
 
-        while right:
-            left = left.next
-            right = right.next
+        
+        first.next = first.next.next
 
-        # delete
-        left.next = left.next.next
         return dummy.next
