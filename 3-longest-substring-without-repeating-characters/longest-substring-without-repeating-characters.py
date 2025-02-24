a2afaf46c08802a0878c919b765a6716f519e776
Dhaval Patel <dhavaljp.95@gmail.com>
2025-02-24 00:11:49 -0500

    Time: 19 ms (57.12%) | Memory: 17.9 MB (52.96%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,15 +1,61 @@
+'''
+Approach
+maintain a set for the unique characters
+2 pointers, left and right, increase the size of the window if the new char is not part of the set
+if the new char is part of the set, reduce the len of the window till the set no longer contain the char
+add the new char and maintain the len of the subarry
+return the max len of the subarray without duplicate characters
+
+Edge Case:
+
+if len(s) == 0:
+    return 0
+
+if len(s) == 1:
+    return 1 
+
+
+L, R = 0,1
+maxLen = 1
+charSet = set()
+while R < len(s):
+    if s[R] not in charSet:
+        charSet.add(S[R])
+        currLen = R - L + 1
+        if currLen > maxLen:
+            maxLen = currLen
+    else:
+        while s[R] not in charSet:
+            charSet.remove(s[L])
+            L += 1
+        
+    R +=1
+
+return maxLen
+
+Algo:
+
+
+'''
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        charSet = set()
-        l = 0
-        res = 0
+        if len(s) == 0:
+            return 0
 
-        for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
-                l+=1
-            charSet.add(s[r])
-            res = max(res, r-l+1)
-        
-        return res
+        if len(s) == 1:
+            return 1 
 
+        L, R = 0, 0
+        max_len = 0
+        char_set = set()
+
+        while R < len(s):
+            if s[R] not in char_set:
+                char_set.add(s[R])
+                max_len = max(max_len, R - L + 1)
+                R += 1
+            else:
+                char_set.remove(s[L])
+                L += 1
+
+        return max_len
