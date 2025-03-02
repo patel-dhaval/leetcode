75a1a13fcac803bf0436ba6943ca7bd20edf5bca
Dhaval Patel <dhavaljp.95@gmail.com>
2025-03-02 12:24:22 -0500

    Time: 41 ms (12.69%) | Memory: 20.6 MB (6.26%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,51 +1,37 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        
         if not grid:
-            return 0
+            return 0        
+        
+        visit = set()
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        max_area = 0
 
-        rows, cols = len(grid), len(grid[0])
-        visited=set()
-        maxSum = 0
+        def dfs(r, c):
+            visit.add((r,c))
+            area = 1
+            directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
 
-        def dfs(r,c) -> int:
-            visited.add((r,c))
-            row, col = r,c
-            directions= [[1,0],[-1,0],[0,1],[0,-1]]
-            sum = 1
-            for dr,dc in directions:
-                r,c = row + dr, col + dc
-                if (r) in range(rows) and (c) in range(cols) and grid[r][c] == 1 and (r ,c) not in visited:
-                    sum += dfs(r, c)
-            return sum
+            for dr, dc in directions:
+                row = r + dr
+                col = c + dc
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1 and (r,c) not in visited:
-                    maxSum = max(dfs(r,c), maxSum)
+                if (min(row, col) < 0 
+                    or row == ROWS 
+                    or col == COLS 
+                    or grid[row][col] == 0
+                    or (row,col) in visit):
+                    continue
+                area += dfs(row, col)
+            return area
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) not in visit and grid[r][c] == 1:
+                    current_area = dfs(r, c)
+                    print(current_area)
+                    if current_area > max_area:
+                        max_area = current_area
 
-        return maxSum
-
-        # if not grid:
-        #     return 0
-
-        # rows, cols = len(grid), len(grid[0])
-        # visited = set()
-        # maxSum = 0
-
-        # def dfs(r, c) -> int:
-        #     visited.add((r, c))
-        #     sum = 1
-        #     directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        #     for dr, dc in directions:
-        #         nr, nc = r + dr, c + dc
-        #         if (nr) in range(rows) and (nc) in range(cols) and grid[nr][nc] == 1 and (nr, nc) not in visited:
-        #             sum += dfs(nr, nc)  # Add the result of recursive dfs call to the current sum
-        #     return sum
-
-        # for r in range(rows):
-        #     for c in range(cols):
-        #         if grid[r][c] == 1 and (r, c) not in visited:
-        #             maxSum = max(dfs(r, c), maxSum)
-
-        # return maxSum
+        return max_area
