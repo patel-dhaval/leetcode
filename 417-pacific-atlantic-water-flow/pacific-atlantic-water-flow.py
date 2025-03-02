0a28042b3db23741eaadb4c366404a5176d49aaa
Dhaval Patel <dhavaljp.95@gmail.com>
2025-03-02 18:05:03 -0500

    Time: 43 ms (34.41%) | Memory: 19.4 MB (64.07%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,47 +1,46 @@
-from typing import List
-
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        row_count = len(heights)
-        col_count = len(heights[0])
-        res = []
-        visited_atl = set()
+        grid = heights
+        ROWS, COLS = len(grid), len(grid[0])
         visited_pac = set()
+        visited_atl = set()
+        solution = []
 
-        directions = [[0, -1], [0, 1], [1, 0], [-1, 0]]
+        def dfs(r, c, visited):
+            if (r, c) in visited:
+                return
 
+            visited.add((r,c))
 
-        def dfs_atl(r, c):
-            visited_atl.add((r, c))
+            directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
+
             for dr, dc in directions:
-                row, col = r + dr, c + dc
-                if (0 <= row < row_count and
-                    0 <= col < col_count and
-                    heights[row][col] >= heights[r][c] and
-                    (row, col) not in visited_atl):
-                    dfs_atl(row, col)
+                row = r+dr
+                col = c+dc
 
-        def dfs_pac(r, c):
-            visited_pac.add((r, c))
-            for dr, dc in directions:
-                row, col = r + dr, c + dc
-                if (0 <= row < row_count and
-                    0 <= col < col_count and
-                    heights[row][col] >= heights[r][c] and
-                    (row, col) not in visited_pac):
-                    dfs_pac(row, col)
+                if min(row, col) < 0 or row == ROWS or col == COLS or (grid[r][c] > grid[row][col]):
+                    continue
+                dfs(row, col, visited)
 
-        for c in range(col_count):
-            dfs_pac(0, c)
-            dfs_atl(row_count - 1, c)
+        
+        for c in range(COLS):
+            dfs(0, c, visited_pac)
+        
+        for r in range(ROWS):
+            dfs(r, 0, visited_pac)
+        
+        for c in range(COLS):
+            dfs(ROWS-1, c, visited_atl)
+    
+        for r in range(ROWS):
+            dfs(r, COLS-1, visited_atl)
 
-        for r in range(row_count):
-            dfs_pac(r, 0)
-            dfs_atl(r, col_count - 1) 
-
-        for r in range(row_count):
-            for c in range(col_count):
-                if (r, c) in visited_atl and (r, c) in visited_pac:
-                    res.append([r, c])
-
-        return res
+        # print("PC", visited_pac)
+        # print("ATL", visited_atl)
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in visited_atl and (r,c) in visited_pac:
+                    solution.append([r,c])
+        
+        return solution
