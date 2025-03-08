836280f77db8d3df23fe6502a63b7073303f9ff7
Dhaval Patel <dhavaljp.95@gmail.com>
2025-03-08 12:33:17 -0500

    Time: 15 ms (54.49%) | Memory: 19.3 MB (69.86%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,3 +1,34 @@
+"""
+Clarifying questions
+Before the first and the last input, are we considering that the elevation is zero?
+Only positive Heights considered
+
+
+Edge Cases
+First and Last positions will never hold any water if the 0th and the last + 1 positions are at zero elevation
+
+Approach
+We would use two pointers. Tracking of the max Left and the max Right values
+At every position, we will calculate how much water can be stored - water : min(maxL, maxR) - height
+if height > 0, add, else, 0
+move either L or R depending on whichever is smaller
+Keep processing till l < r
+Once this condition breaks, we know that we have successfully processed all input and we will have the solution
+
+Algo
+
+Dry Run
+
+
+TC
+
+
+Sc
+
+
+"""
+
+
 class Solution:
     def trap(self, height: List[int]) -> int:
         if len(height) < 3:
