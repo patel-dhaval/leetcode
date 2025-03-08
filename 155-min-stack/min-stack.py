50732661dee862fabdfb8f8ea732fe96af447bf8
Dhaval Patel <dhavaljp.95@gmail.com>
2025-03-08 18:55:19 -0500

    Time: 6 ms (56.71%) | Memory: 22 MB (6.78%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,64 +1,24 @@
-# class MinStack:
-
-#     def __init__(self):
-#         self.stack = []
-
-#     def push(self, val: int) -> None:
-#         if self.stack:
-#             self.minVal = self.stack[-1][1]
-#         else:
-#             self.minVal = float('inf')
-#         if val < self.minVal:
-#             self.minVal = val  
-#         self.stack.append([val, self.minVal])
-#         return None
-
-#     def pop(self) -> None:
-#         self.stack.pop()
-#         return None
-
-#     def top(self) -> int:
-#         return self.stack[-1][0]
-
-#     def getMin(self) -> int:
-#         minval = self.stack[-1][1]
-#         return minval
-
-
 class MinStack:
 
     def __init__(self):
-        self.stk1=[]
-        self.stk2=[inf]
-        
-        
+        self.s=[]
 
     def push(self, val: int) -> None:
-        self.stk1.append(val)
-        self.stk2.append(min(val,self.stk2[-1]))
-        
+        min_val=self.getMin()
+        if min_val==None or min_val>val:
+            min_val=val
+        self.s.append([val,min_val])
 
     def pop(self) -> None:
-        self.stk1.pop()
-        self.stk2.pop()
-
-        
+        self.s.pop()
 
     def top(self) -> int:
-        return self.stk1[-1]
-        
+        return self.s[-1][0] if self.s else None
 
     def getMin(self) -> int:
-        return self.stk2[-1]
-        
+        return self.s[-1][1] if self.s else None
 
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
