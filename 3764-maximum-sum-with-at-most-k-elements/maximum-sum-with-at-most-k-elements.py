960c7e76623b6efd7e4b0225cc3d424eaf1d22a4
Dhaval Patel <dhavaljp.95@gmail.com>
2025-03-09 15:41:45 -0400

    Time: 186 ms (61.76%) | Memory: 46.7 MB (85.68%) - LeetSync

--- 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.py
+++ 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.py
@@ -1,26 +1,17 @@
+from heapq import heappush, heappop
+from typing import List
+
 class Solution:
     def maxSum(self, grid: List[List[int]], limits: List[int], k: int) -> int:
-        def heap_calc(min_heap, k, arr):
-            for num in arr:
-                heapq.heappush(min_heap, num)
-                if len(min_heap) > k:
-                    heapq.heappop(min_heap)
-            return min_heap[:k]
-
-        combined = []
-        i = 0
-        for row in grid:
-            combined = combined + (heap_calc([], limits[i],row))
-            i+=1
-        
         min_heap = []
-        for num in combined:
-            heapq.heappush(min_heap, num)
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
-            
-        sol_sum = 0
-        while len(min_heap):
-            sol_sum = (heapq.heappop(min_heap)) + sol_sum
 
-        return sol_sum
+        for i, row in enumerate(grid):
+            # Sort the row and take the largest 'limits[i]' numbers
+            top_elements = sorted(row, reverse=True)[:limits[i]]
+            
+            for num in top_elements:
+                heappush(min_heap, num)
+                if len(min_heap) > k:
+                    heappop(min_heap)  # Keep only the top k elements
+
+        return sum(min_heap)
