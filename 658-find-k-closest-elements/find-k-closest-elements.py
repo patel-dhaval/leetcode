ac53f5af17882e2f67c8f53184a297251e63859f
Dhaval Patel <dhavaljp.95@gmail.com>
2025-03-15 15:30:37 -0400

    Time: 2 ms (75.01%) | Memory: 18.9 MB (82.77%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -9,16 +9,34 @@ mod_diff = 1, 0, 1
 
 res = [1,2]
 """
+# class Solution:
+#     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+#         pq = []
+#         nums = arr
+#         for num in nums:
+#             dist = abs(num - x)
+#             heapq.heappush(pq, (-dist, -num))  # Push negative to simulate max-heap
+#             if len(pq) > k:
+#                 heapq.heappop(pq)
+
+#         res = [-heapq.heappop(pq)[1] for _ in range(k)]
+#         res.sort()
+#         return res
+
+
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        pq = []
-        nums = arr
-        for num in nums:
-            dist = abs(num - x)
-            heapq.heappush(pq, (-dist, -num))  # Push negative to simulate max-heap
-            if len(pq) > k:
-                heapq.heappop(pq)
+        # Initialize binary search bounds
+        left = 0
+        right = len(arr) - k
+        
+        # Binary search against the criteria described
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
 
-        res = [-heapq.heappop(pq)[1] for _ in range(k)]
-        res.sort()
-        return res
+        return arr[left:left + k]
+
