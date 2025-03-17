a1c82a53b1db530d0b60274ac84eb2bc167d0366
Dhaval Patel <dhavaljp.95@gmail.com>
2025-03-17 16:23:28 -0400

    Time: 41 ms (66.52%) | Memory: 18.3 MB (11.93%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -7,34 +7,36 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-	    # DFS function to go through the tree and mark the parent of each node in a dictionary
-        def getParent(node, parent):
-            if node is None:
-                return 
+        parentNodes = {}
 
-            parentMap[node] = parent # mark parent of each node
+        def getParent(node, parent):
+            if not node:
+                return
+
+            parentNodes[node] = parent
 
             getParent(node.left, node)
             getParent(node.right, node)
-        
-        # DFS function that gets the nodes at a distance k from the target
-        def getNodes(node, cnt):
-            if not node or node in seen or cnt > k:
-                return 
 
-            seen.add(node)
-            if cnt == k:
-                res.append(node.val) # append to result when cnt/distance is k
-
-            getNodes(node.left, cnt + 1)
-            getNodes(node.right, cnt + 1)
-            getNodes(parentMap[node], cnt + 1)
-
-        parentMap = {}
-        seen = set()
+        visited = set()
+        distance = k
         res = []
-        getParent(root, None) # call getParent func for 'root' node
-        getNodes(target, 0) # call getNodes func for 'target' node
+
+        def dfs(node, distance):
+            if not node or node in visited or distance < 0:
+                return None
+
+            visited.add(node)
+            
+            if distance == 0:
+                res.append(node.val)
+
+            dfs(node.left, distance-1)
+            dfs(node.right, distance-1)
+            dfs(parentNodes[node], distance - 1)
+
+
+        getParent(root, None)
+        dfs(target, k)
 
         return res
-            
