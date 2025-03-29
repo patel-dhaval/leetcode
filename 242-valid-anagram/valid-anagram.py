e644b5e168ded29bd2c356ae18c92b3d55193a19
Dhaval Patel <dhavaljp.95@gmail.com>
2025-03-29 18:06:52 -0400

    Time: 11 ms (72.51%) | Memory: 17.8 MB (64.51%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,22 @@
+"""
+Approach: For 2 strings to be anagrams, they need to have the exact combination of characters. 
+Thus, using a frequency array would be good. 
+Instead of using 2 freq arrays and then comparing, we can use just 1, adding 1 to the existing value if the char is in s and subtracting 1 if the value is in t. 
+Once all the input is processed, check freq array, if any non-zero element found then not anagrams, else anagrams
+"""
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if(len(s) == len(t)):
-            hmap_s = {}
-            for i in range(0, len(s)):
-                if s[i] in hmap_s:
-                    hmap_s[s[i]] = (hmap_s[s[i]] + 1)
-                else:
-                    hmap_s[s[i]] = 1
-            hmap_t = {}
-            for i in range(0, len(t)):
-                if t[i] in hmap_t:
-                    hmap_t[t[i]] = (hmap_t[t[i]] + 1)
-                else:
-                    hmap_t[t[i]] = 1
-                
-            if hmap_s == hmap_t:
-                return True
-        return False
+        
+        if len(s) != len(t):
+            return False
+
+        freq_arr = [0] * 26
+
+        for i in range(len(s)):
+            freq_arr[ord(s[i]) - ord('a')] +=1
+            freq_arr[ord(t[i]) - ord('a')] -=1
+        
+        for i in freq_arr:
+            if i != 0:
+                return False
+        return True
