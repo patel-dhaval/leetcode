06a62b2d433bebe48c794ecccd213d6528dbb9c2
Dhaval Patel <dhavaljp.95@gmail.com>
2025-03-29 19:27:53 -0400

    Time: 2 ms (15.58%) | Memory: 17.6 MB (83.15%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,46 +1,30 @@
+"""
+Approach:
+use low, mid and high
+
+if ip == 0: swap low with mid, and increment both low and mid
+if ip == 1: increment mid
+if ip == 2: swap with mid and decement high
+
+constraint is that mid <= high
+"""
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # O(N)
-
         low = 0
-        mid = 0
         high = len(nums) - 1
+        mid = 0
 
         while mid <= high:
             if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                low += 1
-                mid += 1
+                nums[mid], nums[low] = nums[low], nums[mid]
+                low +=1
+                mid +=1
             elif nums[mid] == 1:
                 mid += 1
-            else:
+            elif nums[mid] == 2:
                 nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
-
-
-        # # O(2N)
-
-        # arr = nums
-        # cnt0 = 0
-        # cnt1 = 0
-        # cnt2 = 0
-
-        # for num in arr:
-        #     if num == 0:
-        #         cnt0 += 1
-        #     elif num == 1:
-        #         cnt1 += 1
-        #     else:
-        #         cnt2 += 1
-
-        # for i in range(cnt0):
-        #     arr[i] = 0
-
-        # for i in range(cnt0, cnt0 + cnt1):
-        #     arr[i] = 1
-
-        # for i in range(cnt0 + cnt1, len(arr)):
-        #     arr[i] = 2
+        
