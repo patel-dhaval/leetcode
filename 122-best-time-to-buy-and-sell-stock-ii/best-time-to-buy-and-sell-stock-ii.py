80472435c736d93dd6a20594d5a3d4525605141f
Dhaval Patel <dhavaljp.95@gmail.com>
2025-04-02 23:32:37 -0400

    Time: 0 ms (100.00%) | Memory: 18.9 MB (28.31%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         currently_holding = False
         profit = 0
-        total_profit = 0
         curr_pp = 0
 
         for i in range(len(prices)):
@@ -12,14 +11,12 @@ class Solution:
                     currently_holding = True
                     # print(curr_pp)
                 elif (prices[i] > prices[i+1] and currently_holding):
-                    profit = prices[i] - curr_pp
+                    profit += prices[i] - curr_pp
                     # print("profit", profit)
                     currently_holding = False
                     curr_pp = 0
-                    total_profit += profit
             elif currently_holding and i == len(prices)-1:
-                profit = prices[i] - curr_pp
+                profit += prices[i] - curr_pp
                 currently_holding = False
                 curr_pp = 0
-                total_profit += profit
-        return total_profit
+        return profit
