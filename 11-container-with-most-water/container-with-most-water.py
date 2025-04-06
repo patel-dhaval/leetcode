f8dba6e778070a3f897a804dcc8b4172329e3ecf
Dhaval Patel <dhavaljp.95@gmail.com>
2025-04-05 20:37:07 -0400

    Time: 91 ms (78.89%) | Memory: 28.3 MB (98.17%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,24 +1,12 @@
-'''
-Approach
-2 Pointers, calc volume - dist * min(heights1, heights2)
-if h1 < h2:
-    h1 +=1
-else:
-    h2 -= 1
-keep tracking max volume 
-break condiiton when h1 > h2
-
-'''
-
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        L, R = 0, len(height)-1
-        maxVol = 0
-        while L < R:
-            vol = (R - L) * min(height[L], height[R])
-            maxVol = max(vol, maxVol)
-            if height[L] < height[R]:
-                L += 1
+        l, r = 0, len(height)-1
+        max_vol = 0
+        while l < r:
+            vol = min(height[l], height[r]) * (r - l)
+            max_vol = max(vol, max_vol)
+            if height[l] > height[r]:
+                r -= 1
             else:
-                R -= 1
-        return maxVol
+                l += 1
+        return max_vol
