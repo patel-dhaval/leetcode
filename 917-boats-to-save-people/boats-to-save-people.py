124f2a6f7d3488ee0ba5fb3f155e615a4a0bc9b2
Dhaval Patel <dhavaljp.95@gmail.com>
2025-04-05 21:22:43 -0400

    Time: 67 ms (7.32%) | Memory: 22.3 MB (99.06%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -1,6 +1,28 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        people.sort()
+        m = max(people)
+        counts = [0] * (m+1)
+
+        # Count the quantity of each val in arr
+        for p in people:
+            counts[p] += 1
+        
+        #print(counts)
+        # Fill each bucket in the original array
+        idx, i = 0, 1
+        while idx < len(people):
+            while counts[i] == 0:
+                i += 1
+            people[idx] = i
+            counts[i] -= 1
+            idx += 1
+
+        # i = 0
+        # for idx in range(len(people)):
+        #     for _ in range(counts[idx]):
+        #         people[i] = idx
+        #         i+=1
+
         l, r = 0, len(people)-1
 
         res, l, r = 0, 0, len(people) - 1
