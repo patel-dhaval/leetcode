853971ffd9fd69004288142f978cc4746e7953cb
Dhaval Patel <dhavaljp.95@gmail.com>
2025-04-10 00:16:49 -0400

    Time: 37 ms (81.65%) | Memory: 27.9 MB (95.53%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -4,11 +4,12 @@ class Solution:
         l = 0
         temp_sum = 0
         temp_avg = 0
+        target_sum = k * threshold
         for r in range(len(arr)):
             temp_sum += arr[r]
             if (r - l +1) == k:
-                temp_avg = temp_sum//(r-l+1)
-                if temp_avg >= threshold:
+                # temp_avg = temp_sum//(r-l+1)
+                if temp_sum >= target_sum:
                     max_subarrays += 1
                 temp_sum -= arr[l]
                 l += 1
