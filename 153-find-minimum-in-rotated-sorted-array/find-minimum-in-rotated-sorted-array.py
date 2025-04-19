d6f41f870192908d4041e14186447a81823041b2
Dhaval Patel <dhavaljp.95@gmail.com>
2025-04-19 19:41:13 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (50.31%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,32 +1,32 @@
-"""
-Clarifying questions
-
-Edge cases
-
-Approach
-
-Code
-
-TC
-
-SC
-
-"""
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        L , R = 0, len(nums) - 1 
+        l = 0
+        r = len(nums) - 1
         curr_min = float("inf")
-        
-        while L  <  R :
-            mid = L + (R - L ) // 2
-            curr_min = min(curr_min,nums[mid])
-            
-            # right has the min 
-            if nums[mid] > nums[R]:
-                L = mid + 1
-                
-            # left has the  min 
+        while l <= r:
+            m = (l + r) // 2
+            curr_min = min(curr_min, nums[m])
+            if nums[m] < nums[r]:
+                r = m - 1
             else:
-                R = mid - 1 
+                l = m + 1
+        
+        return curr_min
+
+
+        # L , R = 0, len(nums) - 1 
+        # curr_min = float("inf")
+        
+        # while L  <  R :
+        #     mid = L + (R - L ) // 2
+        #     curr_min = min(curr_min,nums[mid])
+            
+        #     # right has the min 
+        #     if nums[mid] > nums[R]:
+        #         L = mid + 1
                 
-        return min(curr_min,nums[L])
+        #     # left has the  min 
+        #     else:
+        #         R = mid - 1 
+                
+        # return min(curr_min,nums[L])
