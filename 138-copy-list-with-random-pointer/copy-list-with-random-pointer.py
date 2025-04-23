bd07e036f9f28f0c39be60bf6416620e32c5c27f
Dhaval Patel <dhavaljp.95@gmail.com>
2025-04-23 00:22:12 -0400

    Time: 39 ms (73.25%) | Memory: 18.7 MB (19.55%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -7,15 +7,6 @@ class Node:
         self.random = random
 """
 
-"""
-Approach:
-2 Pass approach with Hmap
-
-First pass, create the deep copy nodes and map it against the current nodes. This pass will build the nodes
-
-Second Pass, create the linkage, both random pointers and next pointer, on the basis of current head. Keep moving till the end of LL has reached (next pointing to null).
-"""
-
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
@@ -23,19 +14,19 @@ class Solution:
         old_to_new = {None:None}
         
         curr = head
-
         while curr:
-            copy_node = Node(curr.val)
-            old_to_new[curr] = copy_node
+            newNode = Node(curr.val)
+            old_to_new[curr] = newNode
             curr = curr.next
-        
+
         curr = head
 
         while curr:
-            copy_node = old_to_new[curr]
-            copy_node.next = old_to_new[curr.next]
-            copy_node.random = old_to_new[curr.random]
+            newNode = old_to_new[curr]
+            if curr.next:
+                newNode.next = old_to_new[curr.next]
+            if curr.random:
+                newNode.random = old_to_new[curr.random]
             curr = curr.next
-        
-        return old_to_new[head]
 
+        return old_to_new[head]
