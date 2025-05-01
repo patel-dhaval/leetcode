fe4fc9da91c559d9630671f34e4805d6dbb96cd2
Dhaval Patel <dhavaljp.95@gmail.com>
2025-04-30 23:23:06 -0400

    Time: 7 ms (47.53%) | Memory: 20 MB (10.07%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,23 +1,82 @@
+# class Solution:
+#     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+#         toposort = []
+#         adjList = {}
+#         visit = set()
+#         path_visit = set()
+
+#         for src, dst in prerequisites:
+#             if src not in adjList:
+#                 adjList[src] = [dst]
+#             else:
+#                 adjList[src].append(dst)
+#             if dst not in adjList:
+#                 adjList[dst] = []
+        
+
+#         def dfs(node):
+#             if node in visit:
+#                 return True
+            
+#             if node in path_visit:
+#                 return False
+            
+            
+#             path_visit.add(node)
+
+#             for nei in adjList[node]:
+#                 if not dfs(nei):
+#                     return False
+            
+#             toposort.append(node)
+#             visit.add(node)
+#             path_visit.remove(node)
+            
+#             return True
+        
+#         for node in adjList.keys():
+#             if not dfs(node):
+#                 return False
+        
+#         print(toposort)
+        
+#         if len(toposort) != numCourses:
+#             return False
+        
+#         return True
+            
+
+
+from typing import List
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0] * numCourses
-        adj = [[] for i in range(numCourses)]
+        adjList = {i: [] for i in range(numCourses)}
         for src, dst in prerequisites:
-            indegree[dst] += 1
-            adj[src].append(dst)
+            adjList[src].append(dst)
 
-        q = deque()
-        for n in range(numCourses):
-            if indegree[n] == 0:
-                q.append(n)
-        
-        finish = 0
-        while q:
-            node = q.popleft()
-            finish += 1
-            for nei in adj[node]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    q.append(nei)
-                
-        return finish == numCourses
+        visit = set()
+        path_visit = set()
+
+        def dfs(node):
+            if node in path_visit:
+                return False
+
+            if node in visit:
+                return True
+
+            path_visit.add(node)
+
+            for neighbor in adjList[node]:
+                if not dfs(neighbor):
+                    return False
+
+            path_visit.remove(node)
+            visit.add(node)
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+
+        return True
