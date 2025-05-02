8cb739acc48b4fed6869acc9e232fcc6940888cf
Dhaval Patel <dhavaljp.95@gmail.com>
2025-05-02 00:08:33 -0400

    Time: 86 ms (30.98%) | Memory: 22.6 MB (63.94%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -0,0 +1,16 @@
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        max_heap = []
+        heapq.heapify(max_heap)
+        res = []
+        for x,y in points:
+            dist = x**2 + y**2
+            heapq.heappush(max_heap, [-dist, [x,y]])
+            if len(max_heap) > k:
+                heapq.heappop(max_heap)
+        
+        for v in max_heap:
+            res.append(v[1])
+
+        return res
