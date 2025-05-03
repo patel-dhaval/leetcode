3a7935762820dd976a7f2858a4ed9c7c6ca9b92b
Dhaval Patel <dhavaljp.95@gmail.com>
2025-05-03 19:10:00 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.21%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -2,15 +2,17 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
         def subsets(index, currSet, res):
-            if index >= len(nums):
-                res.append(currSet.copy())
+            
+            if index > len(nums):
                 return
-        
-            currSet.append(nums[index])
-            subsets(index+1, currSet, res)
-            currSet.pop()
-            subsets(index+1, currSet, res)
 
+            res.append(currSet.copy())
+            
+            for i in range(index, len(nums)):
+                currSet.append(nums[i])
+                subsets(i+1, currSet, res)
+                currSet.pop()
+            
             return res
         
         return subsets(0, [], [])
