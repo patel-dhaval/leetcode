0edb2775514b2c43b8d0781c1e811827bad79e4e
Dhaval Patel <dhavaljp.95@gmail.com>
2025-05-02 23:30:42 -0400

    Time: 151 ms (20.11%) | Memory: 19.3 MB (21.36%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,29 +1,28 @@
-"""
-Approach
-
-Since we have frequencies of tasks, we can use heap as it will give us the max of the freq in log time
-There is a timeout which is present, after which the same 2 tasks can be processed.
-To track this time, we can use a queue, where we add the updated freq of the task as well as the time when it will be avaialbe to be processed
-Loop through till there are tasks in heap or queue
-Track time intervals
-If time interval == queue[0][1]: pop the value from queue and push it up on heap
-
-"""
-
+import heapq
+from collections import Counter
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        count = Counter(tasks)
-        maxHeap = [-cnt for cnt in count.values()]
-        heapq.heapify(maxHeap)
-
+        max_heap = []
+        heapq.heapify(max_heap)
+        temp = deque()
+        val = Counter(tasks)
+    
+        for k, v in val.items():
+            heapq.heappush(max_heap, [-v, k, 0])
+        
         time = 0
-        q = deque()  # pairs of [-cnt, idleTime]
-        while maxHeap or q:
+        while max_heap or temp:
             time += 1
-            if maxHeap:
-                cnt = 1 + heapq.heappop(maxHeap)
-                if cnt:
-                    q.append([cnt, time + n])
-            if q and q[0][1] == time:
-                heapq.heappush(maxHeap, q.popleft()[0])
+            if max_heap:
+                v, k, time_passed = heapq.heappop(max_heap)
+                v = abs(v)
+                v -= 1
+                if v != 0:
+                    temp.append([-v, k, time+n])
+
+            if temp and temp[0][2] == time:
+                temp_val = temp.popleft()
+                heapq.heappush(max_heap, temp_val)
+    
         return time
+        
