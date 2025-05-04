102d120a82cf04bd55c7808cbf0753aa8e6d6c73
Dhaval Patel <dhavaljp.95@gmail.com>
2025-05-04 19:44:46 -0400

    Time: 19 ms (87.01%) | Memory: 23.3 MB (53.86%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,33 +1,16 @@
-'''
-Prefix & Postfix on the input arr to calculate product of arr except self
-
-Prefix
-
-inialize prefix factor to 1
-Set first position as 1
-carry out operation and update the prefix factor
-proceed till length of prefix == input
-
-Posfix:
-
-initialize the factor to 1
-Carry out operation from end of the input till the start
-Update the factor by mulitplying with the input at that curr position
-
-'''
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pre, post = 1, 1
-
+        prefix = 1
         res = []
+        res.append(prefix)
+        for i in range(len(nums)-1):
+           res.append(prefix * nums[i])
+           prefix = prefix * nums[i]
 
-        for i in range(0 , len(nums)):
-            res.append(pre)
-            pre = pre * nums[i]
-        
-        for i in range(len(nums)-1, -1, -1):
-            res[i] = res[i] * post
-            post = post * nums[i]
+        postfix = 1
+
+        for j in range(len(nums)-1, -1, -1):
+            res[j] = res[j] * postfix
+            postfix = postfix * nums[j]
         
         return res
