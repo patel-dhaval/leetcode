a1684dce34b7768ceea7351a29ad9557e87c443b
Dhaval Patel <dhavaljp.95@gmail.com>
2025-05-03 23:35:39 -0400

    Time: 7378 ms (6.16%) | Memory: 17.9 MB (39.82%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,44 +1,37 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        if not board:
-            return False
-
         ROWS = len(board)
         COLS = len(board[0])
+        visit = set()
         
-        def dfs(r, c, idx):
-            
-            if (r,c) in visited:
+        word_list = list(word)
+        def word_seach(r, c, idx):
+            if (r,c) in visit:
                 return False
-
+            
             if idx == len(word) -1:
                 return True
 
-            index = idx+1
-
-            visited.add((r, c))
-
-            directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
+            visit.add((r,c))
+            
+            directions = [[1,0], [-1, 0], [0,1], [0,-1]]
 
             for dr, dc in directions:
                 row = r + dr
-                col = c + dc 
-
-                if 0 <= row < ROWS and 0 <= col < COLS and index < len(word) and board[row][col] == word[index] and (row, col) not in visited:
-                    if dfs(row, col, index):
-                        return True
-                
-            visited.remove((r,c))
-
+                col = c + dc
+                next_index = idx + 1
+                if min(row, col) < 0 or row == ROWS or col == COLS or (row, col) in visit or next_index >= len(word) or board[row][col] != word[next_index]:
+                    continue
+                if word_seach(row, col, next_index):
+                    return True
+            
+            visit.remove((r,c))
+        
         
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] == word[0]:
-                    visited = set()
-                    if dfs(r, c, 0):
+                    visit = set()
+                    if word_seach(r,c,0):
                         return True
-        
         return False
-        
-
-
