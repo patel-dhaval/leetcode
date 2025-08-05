754b4a492cf1a09f203523daa120ab6d1deeaa89
Dhaval Patel <dhavaljp.95@gmail.com>
2025-08-05 19:54:12 -0400

    Time: 8 ms (86.53%) | Memory: 31.4 MB (90.77%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,16 +1,8 @@
-"""
-Approach: Reduce the number of look-ups to see if duplicate elements are present
-Thus, using a set would be better as the lookup in a set is o(1) operation, for n operations, overall it would remain as o(n)
-Thus, utilize a set for the lookup operations, and if an element found in the set, mark return false and end processing
-"""
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        viewed_elements = set()
-
-        for n in nums:
-            if n not in viewed_elements:
-                viewed_elements.add(n)
-            else:
+        num_set = set()
+        for i in nums:
+            if i in num_set:
                 return True
-        
+            num_set.add(i)
         return False
