c574e205807b97ee4ead9bd729d1a9ffb8388051
Dhaval Patel <dhavaljp.95@gmail.com>
2025-08-05 20:15:24 -0400

    Time: 1 ms (58.56%) | Memory: 18.9 MB (33.09%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,17 +1,9 @@
-"""
-Approach:
-Use a hashmap of the number and index
-check for target - num, if its in map, If present in map, return the index of both the numbers
-If not found, add the current number and its index to the map
-Since its given solution does exists, by the end of the input processing, we will find the result
-"""
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        viewed_numbers = {}
+        processed_nums = {}
 
-        for i in range(len(nums)):
-            potential_pair_number = (target - nums[i])
-            if potential_pair_number in viewed_numbers.keys():
-                return [viewed_numbers[potential_pair_number], i]
+        for i,n in enumerate(nums):
+            if (target - n) in processed_nums.keys():
+                return [i, processed_nums[(target - n)]]
             else:
-                viewed_numbers[nums[i]] = i
+                processed_nums[n] = i
