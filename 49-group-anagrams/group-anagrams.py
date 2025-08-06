28856261c20f6ac05ac2d92a9f5696206cae4da2
Dhaval Patel <dhavaljp.95@gmail.com>
2025-08-05 20:32:35 -0400

    Time: 19 ms (36.19%) | Memory: 22.5 MB (34.81%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,23 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hmap = defaultdict()
-        freq_arr = [0] * 26
-        soln = []
+        result_dict = {}
 
         for s in strs:
-            temp_arr = [0] * 26
-            for i in range(0, len(s)):
-                temp_arr[ord(s[i]) - ord('a')] += 1
-            if tuple(temp_arr) in hmap.keys():
-                temp_val = hmap[tuple(temp_arr)]
-                temp_val.append(s)
-                hmap[tuple(temp_arr)] = temp_val
+            freq_arr = [0] * 26
+            for i in s:
+                freq_arr[ord(i) - ord('a')] += 1
+            if tuple(freq_arr) in result_dict.keys():
+                result_dict[tuple(freq_arr)].append(s)
             else:
-                hmap[tuple(temp_arr)] = [s]
-        
-        for v in hmap.values():
-            soln.append(v)
-        
-        return soln        
+                result_dict[tuple(freq_arr)] = [s]
 
-            
+        return list(result_dict.values())
