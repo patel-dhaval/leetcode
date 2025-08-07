b67e91c1e6004ad14e4694bd25ff2c8d11bc205a
Dhaval Patel <dhavaljp.95@gmail.com>
2025-08-07 00:20:50 -0400

    Time: 23 ms (62.33%) | Memory: 23.3 MB (79.77%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -11,16 +11,31 @@ SC: O(1) if res not included
 """
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res_arr = [1 for _ in range(len(nums))] 
-        temp = 1
-        for i in range(1, len(nums)):
-            res_arr[i] = temp * nums[i-1]
-            temp = temp * nums[i-1]
-        temp = 1
-        for i in range(len(nums)-1, -1, -1):
-            res_arr[i] = temp * res_arr[i]
-            temp =  temp * nums[i]
+        # res_arr = [1 for _ in range(len(nums))] 
+        # temp = 1
+        # for i in range(1, len(nums)):
+        #     res_arr[i] = temp * nums[i-1]
+        #     temp = temp * nums[i-1]
+        # temp = 1
+        # for i in range(len(nums)-1, -1, -1):
+        #     res_arr[i] = temp * res_arr[i]
+        #     temp =  temp * nums[i]
 
-        return res_arr
+        # return res_arr
+
+        prefix = 1
+        res = []
+        res.append(prefix)
+        for i in range(len(nums)-1):
+           res.append(prefix * nums[i])
+           prefix = prefix * nums[i]
+
+        postfix = 1
+
+        for j in range(len(nums)-1, -1, -1):
+            res[j] = res[j] * postfix
+            postfix = postfix * nums[j]
+        
+        return res
 
         
