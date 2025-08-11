4025462dd43a84769c027685368883ae47def5b0
Dhaval Patel <dhavaljp.95@gmail.com>
2025-08-10 20:06:27 -0400

    Time: 211 ms (17.23%) | Memory: 18.2 MB (23.89%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,30 +1,17 @@
-"""
-Approach
-
-Maintain count of the characters
-Keep increasing the size of the window
-Window - max_count <= k
-If this fails, we know that we have to swap more than allowed (k), thus we slide the window and update the count of the element which was removed from the window
-We track the max count and return once all inputs are processed
-
-"""
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
-        if len(s) < 2:
-            return len(s)
-        
-        l, r = 0, 0
-        counts = defaultdict(int)
-        maxLen = 0
-        while r < len(s):
-            counts[s[r]] = 1 + counts.get(s[r], 0)
-            while (r - l + 1) - max(counts.values()) > k:
-                counts[s[l]] -= 1
-                l += 1
-            
-            maxLen = max(maxLen, r - l + 1)
-            r += 1
-        return maxLen
+        L, R = 0,0
+        max_len = float("-inf")
+        char_dict = {}
 
-                
+        for R in range(len(s)):
+            char_dict[s[R]] = char_dict.get(s[R], 0) + 1
+            if (R-L+1 - max(char_dict.values())) <= k:
+                max_len = max(max_len, R - L + 1)
+            
+            else:
+                while (R-L+1 - max(char_dict.values())) > k:
+                    char_dict[s[L]] -= 1
+                    L += 1
+        
+        return max_len
