e451f3760c19ae662305c99d4553adde1b8d19c2
Dhaval Patel <dhavaljp.95@gmail.com>
2025-08-12 23:35:23 -0400

    Time: 140 ms (91.93%) | Memory: 19.2 MB (9.73%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,44 +1,29 @@
-"""
-
-Clarifying questions
-
-Edge cases
-
-Approach
-So the idea is to find the rate, k, at which koko eats bananas from the pile
-this k has to be minimum so that koko can complete eating all the bananas within the specified h hours
-
-Here, brute force would be to check the rates from k = max value within the pile, till 0 to find the minimum k
-But that would be O(n) computation, as for each value of k, we will process the pile.
-
-This can be done better by using binary search.
-We can assume that the lowest rate would fall within the lowest and the largest pile value
-I.E: if we try to search for values within that range and compare with h, we will know which half of the range is likely to have the optimal value of h
-
-This would lower the TC from O(n) to o(log n)
-
-Code
-
-Dry run
-
-TC and SC
-
-"""
-
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        low = 1
-        high = max(piles)
-        min_k = float("inf")
-    
-        while low <= high:
-            mid = (low + high) // 2
+        """
+        We will use binary search to identify the minimum rate
+        Rate would lie between 1 and the max value of bananas in a pile
+        We will compute rates and compare the resultant h
+        Keep continuing till calc_h is lower than h
+
+        TC: O(log n)
+        SC: O(1)
+        """
+
+        min_bananas = 1
+        max_bananas = max(piles)
+
+        while min_bananas < max_bananas:
+            mid = (min_bananas + max_bananas)//2
+
             calc_h = 0
-            for i in piles:
-                calc_h += ceil(i/mid)
-            if calc_h <= h:
-                min_k = min(mid, min_k)
-                high = mid -1
+
+            for pile in piles:
+                calc_h += ceil(pile/mid)
+            
+            if calc_h > h:
+                min_bananas = mid + 1
             else:
-                low = mid + 1
-        return min_k
+                max_bananas = mid
+
+        return min_bananas
