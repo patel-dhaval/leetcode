86decc72102e8d2e365b88308d97e240311ce192
Dhaval Patel <dhavaljp.95@gmail.com>
2025-08-12 23:37:00 -0400

    Time: 158 ms (53.90%) | Memory: 18.8 MB (92.86%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -10,20 +10,42 @@ class Solution:
         SC: O(1)
         """
 
-        min_bananas = 1
-        max_bananas = max(piles)
+        # min_bananas = 1
+        # max_bananas = max(piles)
 
-        while min_bananas < max_bananas:
-            mid = (min_bananas + max_bananas)//2
+        # while min_bananas < max_bananas:
+        #     mid = (min_bananas + max_bananas)//2
 
-            calc_h = 0
+        #     calc_h = 0
 
-            for pile in piles:
-                calc_h += ceil(pile/mid)
+        #     for pile in piles:
+        #         calc_h += ceil(pile/mid)
             
-            if calc_h > h:
-                min_bananas = mid + 1
-            else:
-                max_bananas = mid
+        #     if calc_h > h:
+        #         min_bananas = mid + 1
+        #     else:
+        #         max_bananas = mid
 
-        return min_bananas
+        # return min_bananas
+
+        """
+        Uses binary search to find the minimum bananas/hour Koko needs to eat.
+        
+        Search Space: [1, max(piles)]
+        For each speed, calculate total hours needed and adjust bounds accordingly.
+
+        Time Complexity: O(n log(max(piles)))
+        Space Complexity: O(1)
+        """
+        left, right = 1, max(piles)
+
+        while left < right:
+            mid = left + (right - left) // 2  # Overflow-safe style
+            hours = sum(ceil(pile / mid) for pile in piles)
+
+            if hours > h:
+                left = mid + 1  # Too slow
+            else:
+                right = mid  # Could be slower
+
+        return left
