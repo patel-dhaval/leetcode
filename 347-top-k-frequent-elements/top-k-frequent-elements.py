b9ec75659fe82df1c40dcb64729b1c1a659eabf9
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-03 22:16:54 -0500

    Time: 11 ms (22.70%) | Memory: 24.2 MB (19.71%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,36 +1,18 @@
-"""
-Approach 1:
-freq_arr size n+1
-Sort array
-for each unique element, find the count
-index representing the count, append the value as a list
-
-Parse freq_arr from desc, find values, decrement k, when k == 0, return res
-
-TC: O(nlog n) -> sorting
-SC: O(n)
-
-"""
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        if len(nums) == 1:
-            return nums
-        freq_arr = [[] for _ in range(len(nums) + 1)]
-        nums.sort()
-        cnt = 1
+
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
         res = []
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
-                cnt += 1
-            else:
-                freq_arr[cnt].append(nums[i-1])
-                cnt = 1
-        freq_arr[cnt].append(nums[-1])
-        
-        for num in freq_arr[::-1]:
-            if num:
-                for j in num:
-                    res.append(j)
-                    k-= 1
-                    if k == 0:
-                        return res
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
