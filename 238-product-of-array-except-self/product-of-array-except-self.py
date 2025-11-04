5d125f673ba90b3765819c166df9c63d5af8eb98
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-03 23:23:26 -0500

    Time: 21 ms (70.23%) | Memory: 23.9 MB (39.86%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,41 +1,14 @@
-"""
-Approach:
-precomputation
-prefix and postfix multiplication
-using temp as 1 for both instances
-Left to right, we start building the res arr
-Right to left, we update the res array values
-
-TC: O(n)
-SC: O(1) if res not included
-"""
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # res_arr = [1 for _ in range(len(nums))] 
-        # temp = 1
-        # for i in range(1, len(nums)):
-        #     res_arr[i] = temp * nums[i-1]
-        #     temp = temp * nums[i-1]
-        # temp = 1
-        # for i in range(len(nums)-1, -1, -1):
-        #     res_arr[i] = temp * res_arr[i]
-        #     temp =  temp * nums[i]
-
-        # return res_arr
-
-        prefix = 1
+        prefix_val = 1
+        postfix_val = 1
         res = []
-        res.append(prefix)
-        for i in range(len(nums)-1):
-           res.append(prefix * nums[i])
-           prefix = prefix * nums[i]
-
-        postfix = 1
-
-        for j in range(len(nums)-1, -1, -1):
-            res[j] = res[j] * postfix
-            postfix = postfix * nums[j]
+        res.append(prefix_val)
+        for n in nums[:len(nums)-1]:
+            res.append(prefix_val * n)
+            prefix_val = prefix_val * n
+        for r in range(len(res)-1, -1, -1):
+            res[r] =  res[r] * postfix_val
+            postfix_val = postfix_val * nums[r]
         
         return res
-
-        
