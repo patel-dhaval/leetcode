436b6ca15a7959d8533c57dca50b80f8494ff334
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-04 13:01:14 -0500

    Time: 98 ms (69.69%) | Memory: 28.6 MB (23.85%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,22 +1,12 @@
-"""
-Approach
-2 pointers
-move the pointer which has the smaller value
-compute volume (min-height) * distance
-return max_volume
-"""
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        max_volume = 0
         L, R = 0, len(height) - 1
-
+        max_vol = 0
         while L < R:
             volume = min(height[L], height[R]) * (R - L)
-            max_volume = max(max_volume, volume)
-
-            if height[L] < height[R]:
-                L += 1
-            else:
+            max_vol = max(max_vol, volume)
+            if height[L] > height[R]:
                 R -= 1
-        
-        return max_volume
+            else:
+                L += 1
+        return max_vol
