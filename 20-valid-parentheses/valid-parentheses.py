1a4a646d64289fd86ed3b48c8bf57466cb3cd078
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-05 15:43:37 -0500

    Time: 3 ms (35.39%) | Memory: 17.8 MB (86.31%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,27 +1,15 @@
-"""
-Approach:
-Hashmap of open and close brackets, where closed are keys and open is value
-Use a stack to track the brackets
-When we encounter open bracket, we push it to the stack
-When we encounter closed bracket, we check the top of the stack, if the top of the stack matches the dict value for it, then its a valid series, as it will suggest that an open bracket follows a closed bracket
-
-"""
 class Solution:
     def isValid(self, s: str) -> bool:
-        
         if len(s) < 2:
             return False
-        
-        val_stack = []
-        bracket_dict = {")": "(", "}":"{", "]":"["}
-
-        for i in range(len(s)):
-            if s[i] not in bracket_dict.keys():
-                val_stack.append(s[i])
+        para_map = {"}": "{", ")":"(", "]":'['}
+        stack = []
+        for c in s:
+            if c in para_map.values():
+                stack.append(c)
             else:
-                if val_stack and bracket_dict[s[i]] == val_stack[-1]:
-                    val_stack.pop()
+                if stack and stack[-1] == para_map[c]:
+                    stack.pop()
                 else:
                     return False
-        return True if not val_stack else False
-        
+        return True if not stack else False
