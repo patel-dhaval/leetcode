c387f4769dd65abdd6658a06272ad8dd9f78ca46
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-04 23:45:18 -0500

    Time: 12 ms (88.45%) | Memory: 17.9 MB (52.71%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,17 +1,31 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) < 1:
-            return 0
-        visited_set = set()
+        # if len(s) < 1:
+        #     return 0
+        # visited_set = set()
+        # L = 0
+        # max_len = 1
+        # visited_set.add(s[L])
+        # for R in range(1, len(s)):
+        #     while s[R] in visited_set:
+        #         visited_set.remove(s[L])
+        #         L += 1
+        #     visited_set.add(s[R])
+        #     max_len = max(max_len, len(visited_set))
+
+
+        # return max_len
+
         L = 0
-        max_len = 1
-        visited_set.add(s[L])
-        for R in range(1, len(s)):
-            while s[R] in visited_set:
-                visited_set.remove(s[L])
-                L += 1
-            visited_set.add(s[R])
-            max_len = max(max_len, len(visited_set))
-
-
-        return max_len
+        visited = set()
+        max_count = 0
+        for R in range(len(s)):
+            if s[R] not in visited:
+                visited.add(s[R])
+                max_count = max(max_count, len(visited))
+            else:
+                while s[R] in visited:
+                    visited.remove(s[L])
+                    L+=1
+                visited.add(s[R])
+        return max_count
