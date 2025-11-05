2cf01eca4f84d2675a2afe4321c95b254bd8c99a
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-04 23:22:12 -0500

    Time: 1 ms (49.15%) | Memory: 18.8 MB (69.88%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,22 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        currently_holding = False
-        profit = 0
-        curr_pp = 0
-
-        for i in range(len(prices)):
-            if i < len(prices) - 1:
-                if prices[i] <= prices[i+1] and not currently_holding:
-                    curr_pp = prices[i]
-                    currently_holding = True
-                    # print(curr_pp)
-                elif (prices[i] > prices[i+1] and currently_holding):
-                    profit += prices[i] - curr_pp
-                    # print("profit", profit)
-                    currently_holding = False
-                    curr_pp = 0
-            elif currently_holding and i == len(prices)-1:
-                profit += prices[i] - curr_pp
-                currently_holding = False
-                curr_pp = 0
-        return profit
+        L = 0
+        max_profit = 0
+        for R in range(1, len(prices)):
+            if prices[R] > prices[L]:
+                profit = prices[R] - prices[L]
+                max_profit += profit
+                L = R
+            if prices[R] < prices[L]:
+                L = R
+        return max_profit
