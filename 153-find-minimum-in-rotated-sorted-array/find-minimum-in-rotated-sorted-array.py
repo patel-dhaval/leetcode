6f02465b4762adc4ea57504b81b3010ae29f5da0
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-13 20:02:46 -0500

    Time: 0 ms (100.00%) | Memory: 18.2 MB (8.86%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,26 +1,16 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        """
-        Approach: Use Binary sesarch to identify which half the min would be located
-        This will be done by comparing the middle element with left and right most element.
-        If the middle is greater than the left element, there is still no clarity if its in first or second half, so we compare with right as well, and if the mid is smaller then the right element, that means its in the first half, else second half.
-        Similarly, if the element is smaller than the left element, we still need to further check with the right element, if the mid is greater right element then its in the second half, else its first half
-        """
-
-        left, right = 0, len(nums) - 1
-
-        while left < right:
-            mid = (left + right)//2
-            if nums[mid] > nums[left]:
-                if nums[mid] > nums[right]:
-                    left = mid + 1
+        L, R = 0, len(nums) - 1
+        while L < R:
+            mid = (L+R)//2
+            if nums[mid] > nums[L]:
+                if nums[mid] > nums[R]:
+                    L = mid + 1
                 else:
-                    right = mid
+                    R = mid
             else:
-                if nums[mid] < nums[right]:
-                    right = mid
+                if nums[mid] < nums[R]:
+                    R = mid
                 else:
-                    left = mid + 1
-        return nums[left] 
-
-        
+                    L = mid + 1
+        return nums[L]
