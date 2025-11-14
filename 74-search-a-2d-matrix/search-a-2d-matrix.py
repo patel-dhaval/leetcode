27a5d71ed3a4a56f821991b473a738d3abbf19ef
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-13 19:29:09 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (71.74%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,30 +1,16 @@
-"""
-Approach:
-Lets convert the 2d input into a flat input, since we know the ip conditions, flattening is feasible.
-Identifying row and column would be the tricky bit.
-We can use division and modulo operators
-Div will help identify how many complete rows would fit, and the modulo operator will help find the exact location in the partial row.
-
-Eg:
-
-If we get middle value as 6
-Coordinates would be 6//4 and 6%4 -> 1, 2, thats 11
-"""
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        L = 0
-        
-        rows = len(matrix)
-        cols = len(matrix[0])
-        R = (rows * cols) - 1
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+        L, R = 0, (ROWS * COLS) - 1
 
         while L <= R:
-            mid = (L + R)//2
-            if target > matrix[mid//cols][mid%cols]:
-                L = mid + 1
-            elif target < matrix[mid//cols][mid%cols]:
+            mid = (L+R)//2
+            mid_val = matrix[mid//COLS][mid%COLS]
+            if mid_val > target:
                 R = mid - 1
+            elif mid_val < target:
+                L = mid + 1
             else:
                 return True
-        
         return False
