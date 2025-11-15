a3bd611d5c788f7c6fe283e0e211172236849282
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-15 17:23:07 -0500

    Time: 44 ms (78.93%) | Memory: 19.8 MB (57.65%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -4,14 +4,6 @@
 #         self.val = x
 #         self.next = None
 
-"""
-A technique is used for cycle detection in Linkedlists which is known as slow and fast pointers
-We use 2 pointer, both pointing to head initially, but we move the slow pointer by 1 and fast pointer by 2
-We loop till slow pointer reached the end of the list
-If there is a case that the slow and fast pointers intersect, then we can be sure that there exists a cycle in the List
-
-"""
-
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         slow, fast = head, head
@@ -21,5 +13,5 @@ class Solution:
             fast = fast.next.next
             if slow == fast:
                 return True
-        
+                
         return False
