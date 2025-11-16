acaaca220325a9743e1b710d0fd854a170330224
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-16 18:17:15 -0500

    Time: 7 ms (32.71%) | Memory: 18.1 MB (14.34%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,28 +1,26 @@
-'''
-Approach is based on concept of carry (division by 10 ) and base value (Mod 10)
-Save the base value of the sum and maintain the carry to be added to the sum going forward
-'''
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        temp = dummy
-        carry = 0
-        while (l1 != None or l2 != None) or carry:
-            sum = 0
-            if l1 != None:
-                sum += l1.val
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:        
+        curr_sum = 0 
+        curr_carr = 0
+        dummy = ListNode(0, None)
+        head = dummy
+        while l1 or l2 or curr_carr:
+            curr_sum = curr_carr 
+            if l1:
+                curr_sum += (l1.val)
                 l1 = l1.next
-            if l2 != None:
-                sum += l2.val
+            if l2:
+                curr_sum += (l2.val)
                 l2 = l2.next
-            sum += carry
-            carry = sum // 10
-            node = ListNode(sum % 10)
-            temp.next = node
-            temp = temp.next
-        return dummy.next
+            curr_carr = curr_sum//10
+
+            sum_node = ListNode((curr_sum%10), None)
+            dummy.next = sum_node
+            dummy = dummy.next
+
+        return head.next
