24834c56bc3a283c34e8f5233aef77e8250a1401
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-27 17:20:48 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (59.58%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,36 +1,21 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res_stack = []
-        if not intervals and newInterval:
-            return [newInterval]
-        
-        if intervals and not newInterval:
-            return [intervals]
-
-        new_interval_merged = False
+        n = len(intervals)
         i = 0
-        while i in range(0, len(intervals)):
-            if not new_interval_merged:
-                if newInterval[0] <= intervals[i][1] and newInterval[1] >= intervals[i][0]:
-                    start = min(intervals[i][0], newInterval[0])
-                    end = max(intervals[i][1], newInterval[1])
-                    res_stack.append([start, end])
-                    new_interval_merged = True
-                else:
-                    res_stack.append(intervals[i])
-            elif intervals[i][0] <= res_stack[-1][1] and intervals[i][1] > res_stack[-1][1]:
-                    start = min(res_stack[-1][0], intervals[i][0])
-                    end = max(res_stack[-1][1], intervals[i][1])
-                    res_stack.pop()
-                    res_stack.append([start, end])
-            elif intervals[i][0] <= res_stack[-1][1] and intervals[i][1] <= res_stack[-1][1]:
-                i+=1
-                continue
-            else:
-                res_stack.append(intervals[i])
-            i+=1
+        res = []
 
-        if not new_interval_merged:
-            res_stack.append(newInterval)
-        res_stack.sort()
-        return res_stack
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+
+        while i < n and newInterval[1] >= intervals[i][0]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        res.append(newInterval)
+
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+
+        return res
