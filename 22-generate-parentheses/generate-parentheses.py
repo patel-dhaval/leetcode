8ebb4f721ffa839a40231ee6edacdadc653f44a1
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-26 21:46:02 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (92.56%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,24 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        openN, closeN = 0, 0
         res = []
-        val_stack = []
+        def backtrack(combination, openB, closeB):
+            if openB == closeB == n:
+                temp = "".join(combination.copy())
+                res.append(temp)
+            
+            if openB < n:
+                combination.append("(")
+                backtrack(combination, openB +1 , closeB)
+                combination.pop()
+            
+            if closeB < openB:
+                combination.append(")")
+                backtrack(combination, openB, closeB + 1)
+                combination.pop()
+            
         
-        def backtrack(openN, closeN):
-            if openN == n and closeN == n:
-                res.append("".join(val_stack))
-            
-            if openN < n:
-                val_stack.append('(')
-                backtrack(openN+1, closeN)
-                val_stack.pop()
-            
-            if closeN < openN:
-                val_stack.append(')')
-                backtrack(openN, closeN+1)
-                val_stack.pop()
-
-        backtrack(0, 0)
-
+        backtrack([], 0, 0)
         return res
-
