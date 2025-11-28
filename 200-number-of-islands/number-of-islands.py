e89c93a88768bfc37c4680cc7a690bd21d1d7daa
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-28 18:08:35 -0500

    Time: 293 ms (14.37%) | Memory: 25.4 MB (26.55%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,30 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        visited_set = set()
         ROWS = len(grid)
         COLS = len(grid[0])
-        visit = set()
+        def dfs(r,c):            
+            neighbours = [[0, 1], [0,-1], [1, 0], [-1, 0]]
 
-        def dfs(r, c):
-            
-            visit.add((r,c))
-            directions = [[1,0], [-1, 0], [0,1 ], [0, -1]]
-            
-            for dr, dc in directions:
-                row = r + dr
+            for dr, dc in neighbours:
+                row = r+dr
                 col = c + dc
-
-                if min(row, col) < 0 or row == ROWS or col == COLS or (row, col) in visit or grid[row][col] == '0':
+                if min(row, col) < 0 or row == ROWS or col == COLS or grid[row][col] == '0' or (row, col) in visited_set:
                     continue
-                  
+                visited_set.add((row,col))
                 dfs(row, col)
+            
+            return
 
         count = 0
-        
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == '1' and (r,c) not in visit:
-                    dfs(r, c)
+                if grid[r][c] == '1' and (r,c) not in visited_set:
+                    dfs(r,c)
                     count += 1
         
         return count
-        
