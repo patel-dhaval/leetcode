95f69e931b4a09581ffdfbe095deb250749cd948
Dhaval Patel <dhavaljp.95@gmail.com>
2025-11-29 00:49:31 -0500

    Time: 69 ms (11.54%) | Memory: 20.5 MB (5.55%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,46 +1,51 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        grid = heights
-        ROWS, COLS = len(grid), len(grid[0])
-        visited_pac = set()
-        visited_atl = set()
-        solution = []
+            visited_atl = set()
+            visited_pac = set()
+            ROWS = len(heights)
+            COLS = len(heights[0])
+            res = []
+            def dfs_atl(r,c, visited_atl):            
+                visited_atl.add((r,c))
+                neighbours = [[0, 1], [0,-1], [1, 0], [-1, 0]]
 
-        def dfs(r, c, visited):
-            if (r, c) in visited:
+                for dr, dc in neighbours:
+                    row = r+dr
+                    col = c + dc
+                    if min(row, col) < 0 or row == ROWS or col == COLS or heights[row][col] < heights[r][c]  or (row, col) in visited_atl:
+                        continue
+
+                    dfs_atl(row, col, visited_atl)
+                
+                return
+            
+            def dfs_pac(r,c, visited_pac):            
+                visited_pac.add((r,c))
+                neighbours = [[0, 1], [0,-1], [1, 0], [-1, 0]]
+
+                for dr, dc in neighbours:
+                    row = r+dr
+                    col = c + dc
+                    if min(row, col) < 0 or row == ROWS or col == COLS or heights[row][col] < heights[r][c]  or (row, col) in visited_pac:
+                        continue
+
+                    dfs_pac(row, col, visited_pac)
+                
                 return
 
-            visited.add((r,c))
-
-            directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
-
-            for dr, dc in directions:
-                row = r+dr
-                col = c+dc
-
-                if min(row, col) < 0 or row == ROWS or col == COLS or (grid[r][c] > grid[row][col]):
-                    continue
-                dfs(row, col, visited)
-
-        
-        for c in range(COLS):
-            dfs(0, c, visited_pac)
-        
-        for r in range(ROWS):
-            dfs(r, 0, visited_pac)
-        
-        for c in range(COLS):
-            dfs(ROWS-1, c, visited_atl)
-    
-        for r in range(ROWS):
-            dfs(r, COLS-1, visited_atl)
-
-        # print("PC", visited_pac)
-        # print("ATL", visited_atl)
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r, c) in visited_atl and (r,c) in visited_pac:
-                    solution.append([r,c])
-        
-        return solution
+            for c in range(0, COLS):
+                dfs_pac(0, c, visited_pac)
+                dfs_atl(ROWS-1, c, visited_atl)
+            
+            for r in range(0, ROWS):
+                dfs_pac(r, 0, visited_pac)
+                dfs_atl(r, COLS-1, visited_atl)
+            
+            print(visited_atl)
+            print(visited_pac)
+            for r in range(ROWS):
+                for c in range(COLS):
+                    if (r, c) in visited_atl and (r, c) in visited_pac:
+                        res.append([r,c])
+            
+            return res        
