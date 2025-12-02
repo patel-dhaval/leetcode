600271e738e1e5bbcd6fc64fedb3244f1c7ee867
Dhaval Patel <dhavaljp.95@gmail.com>
2025-12-01 22:21:32 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.35%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,35 +1,37 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        result = []
-        rows, columns = len(matrix), len(matrix[0])
-        up = left = 0
-        right = columns - 1
-        down = rows - 1
+        res = []
+        up, down, left, right = 0, len(matrix)-1, 0, len(matrix[0])-1
+        while len(res) < (len(matrix)* len(matrix[0])): 
+            # left to right
+            for i in range(left, right+1):
+                res.append(matrix[up][i])
 
-        while len(result) < rows * columns:
-            # Traverse from left to right.
-            for col in range(left, right + 1):
-                result.append(matrix[up][col])
+            up+=1
 
-            # Traverse downwards.
-            for row in range(up + 1, down + 1):
-                result.append(matrix[row][right])
+            # down
 
-            # Make sure we are now on a different row.
-            if up != down:
-                # Traverse from right to left.
-                for col in range(right - 1, left - 1, -1):
-                    result.append(matrix[down][col])
+            for j in range(up, down+1):
+                res.append(matrix[j][right])
 
-            # Make sure we are now on a different column.
-            if left != right:
-                # Traverse upwards.
-                for row in range(down - 1, up, -1):
-                    result.append(matrix[row][left])
-
-            left += 1
             right -= 1
-            up += 1
-            down -= 1
 
-        return result
+            # right to left
+
+            if up <= down:
+                for i in range(right, left-1, -1):
+                    res.append(matrix[down][i])
+                
+                down -= 1
+
+            # down to up
+            if left <= right:
+                for j in range(down, up-1, -1):
+                    res.append(matrix[j][left])
+                
+                left += 1
+
+        return res
+
+        
+        
