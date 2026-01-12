bdd6efe1cac444cd0437a039ac76ac8021a76b78
Dhaval Patel <dhavaljp.95@gmail.com>
2026-01-11 20:45:21 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.38%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -16,12 +16,6 @@ class Solution:
                 list2 = list2.next
             dummy = dummy.next
         
-        while list1:
-                dummy.next = list1
-                dummy = dummy.next
-                list1 = list1.next
-        while list2:
-                dummy.next = list2
-                dummy = dummy.next
-                list2 = list2.next
+        dummy.next = list1 or list2
+
         return head.next
