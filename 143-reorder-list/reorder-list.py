57ea0b212a050e28217849996abee1499eb19df4
Dhaval Patel <dhavaljp.95@gmail.com>
2026-01-11 20:55:19 -0500

    Time: 4 ms (35.70%) | Memory: 27.8 MB (6.70%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,34 +8,32 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        curr = head
+
         slow, fast = head, head
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-            if slow == fast:
-                break
-
-        prev = None
-        head2 = slow.next
+        curr2 = slow.next
         slow.next = None
+        prev = None 
+        while curr2:
+            temp = curr2.next
+            curr2.next = prev
+            prev = curr2
+            curr2 = temp
+        
+        head2 = head
+        head3 = prev
 
-        while head2:
-            temp = head2.next
-            head2.next = prev
-            prev = head2
-            head2 = temp
-
-        head3 = head
-        head4 = prev
-
-
-        while head3 and head4:
-            # print(head3.val, head4.val)
-            temp1, temp2 = head3.next, head4.next
-            head3.next = head4
-            head4.next = temp1
-            head3 = temp1
-            head4 = temp2
+        while head2 and head3:
+            temp2 = head2.next
+            temp3 = head3.next
+            head2.next = head3
+            head3.next = temp2
+            head2 = temp2
+            head3 = temp3
         
         return head
