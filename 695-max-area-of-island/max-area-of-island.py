f8f27fa49451c5b7a092496b09157765d7ba78b4
Dhaval Patel <dhavaljp.95@gmail.com>
2026-01-17 18:44:04 -0500

    Time: 39 ms (12.08%) | Memory: 24.3 MB (5.00%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,31 +1,34 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+                
         ROWS = len(grid)
         COLS = len(grid[0])
         visit = set()
         max_area = 0
 
-        def dfs(r, c):
+        def dfs(r, c, area):
             
+            if (r, c) in visit:
+                return
+
             visit.add((r,c))
-            directions = [[1,0], [-1, 0], [0,1 ], [0, -1]]
-            area = 1
-            for dr, dc in directions:
+            neighbours = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            curr_area = 1
+            for dr, dc in neighbours:
                 row = r + dr
                 col = c + dc
 
-                if min(row, col) < 0 or row == ROWS or col == COLS or (row, col) in visit or grid[row][col] == 0:
+                if ( min(row, col) < 0 or row == ROWS or col == COLS  or (row, col) in visit or grid[row][col] == 0):
                     continue
-                  
-                area += dfs(row, col)
-
+                
+                area += dfs(row, col, curr_area)
             return area
+
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1 and (r,c) not in visit:
-                    temp_a = dfs(r, c)
-                    max_area = max(max_area, temp_a)
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (row, col) not in visit and grid[row][col] == 1:
+                    area = dfs(row, col, 1)
+                    max_area = max(max_area, area)
         
         return max_area
-        
