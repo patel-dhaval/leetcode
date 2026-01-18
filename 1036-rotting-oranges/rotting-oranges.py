4002f87dc02223d1961afe46cbbc2e25b2cfda70
Dhaval Patel <dhavaljp.95@gmail.com>
2026-01-17 19:48:30 -0500

    Time: 4 ms (43.67%) | Memory: 19.4 MB (8.66%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,39 +1,43 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        fresh_oranges = 0
+        rotten_oranges = 0
         ROWS = len(grid)
         COLS = len(grid[0])
-        queue = deque()
-        visit = set()
-        fresh, rotten = 0, 0
+        queue = collections.deque()
+
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 2:
-                    rotten += 1
-                    queue.append((r,c))
                 if grid[r][c] == 1:
-                    fresh += 1
+                    fresh_oranges += 1
+                elif grid[r][c] == 2:
+                    queue.append((r,c))
+                    rotten_oranges += 1
 
-        if fresh == 0:
+        if fresh_oranges == 0:
             return 0
 
-        if rotten == 0:
+        if rotten_oranges == 0:
             return -1
 
-        directions = [[1,0], [-1, 0], [0, 1], [0, -1]]
+        neighbours = [[1,0], [-1, 0], [0, 1], [0, -1]] 
         time = 0
         while queue:
             for _ in range(len(queue)):
                 r, c = queue.popleft()
-                visit.add((r,c))
-                for dr, dc in directions:
+
+                for dr, dc in neighbours:
                     row = r + dr
                     col = c + dc
-                    if 0 <= row < ROWS and 0 <= col < COLS and grid[row][col] == 1:
-                        grid[row][col] = 2
-                        queue.append((row, col))
-                        fresh -= 1
-            time += 1
 
-            if fresh == 0:
+                    if min(row, col) < 0 or row == ROWS or col == COLS or grid[row][col] == 0 or grid[row][col] == 2:
+                        continue
+                    
+                    grid[row][col] = 2
+                    queue.append((row, col))
+                    fresh_oranges -= 1
+            
+            time += 1
+            if fresh_oranges == 0:
                 return time
         return -1
