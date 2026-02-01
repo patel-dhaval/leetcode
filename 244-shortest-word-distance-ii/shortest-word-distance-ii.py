ca7fe4047bba2aa103cebec9400d6c49a71fd131
Dhaval Patel <dhavaljp.95@gmail.com>
2026-01-31 21:51:04 -0500

    Time: 7 ms (87.25%) | Memory: 26.4 MB (8.65%) - LeetSync

--- 244-shortest-word-distance-ii/shortest-word-distance-ii.py
+++ 244-shortest-word-distance-ii/shortest-word-distance-ii.py
@@ -1,61 +1,30 @@
+from collections import defaultdict
+
 class WordDistance:
 
     def __init__(self, wordsDict: List[str]):
-        self.wordsDict = wordsDict
-        
+        self.locations = defaultdict(list)
+        for i, word in enumerate(wordsDict):
+            self.locations[word].append(i)
+
     def shortest(self, word1: str, word2: str) -> int:
-        self.location_array_1 = []
-        self.location_array_2 = []
-        self.min_len = float("inf")
-        for i, word in enumerate(self.wordsDict):
-            if word == word1:
-                self.location_array_1.append(i)
-            if word == word2:
-                self.location_array_2.append(i)
+        loc1 = self.locations[word1]
+        loc2 = self.locations[word2]
         
-        i = 0
-        j = 0
+        i, j = 0, 0
+        min_dist = float("inf")
 
-        while i < len(self.location_array_1) and j < len(self.location_array_2):
-            self.min_len = min(self.min_len, abs(self.location_array_1[i] - self.location_array_2[j]))
-            if self.location_array_1[i] < self.location_array_2[j]:
-                i+=1
+        while i < len(loc1) and j < len(loc2):
+            val1, val2 = loc1[i], loc2[j]
+            
+            min_dist = min(min_dist, abs(val1 - val2))
+            
+            if val1 < val2:
+                i += 1
             else:
-                j+=1
+                j += 1
         
-        return self.min_len
-
-# from collections import defaultdict
-
-# class WordDistance:
-
-#     def __init__(self, wordsDict: List[str]):
-#         # OPTIMIZATION: Build the map ONCE here.
-#         self.locations = defaultdict(list)
-#         for i, word in enumerate(wordsDict):
-#             self.locations[word].append(i)
-
-#     def shortest(self, word1: str, word2: str) -> int:
-#         # Retrieve pre-calculated lists
-#         loc1 = self.locations[word1]
-#         loc2 = self.locations[word2]
-        
-#         i, j = 0, 0
-#         min_dist = float("inf")
-
-#         # Two Pointer Logic
-#         while i < len(loc1) and j < len(loc2):
-#             val1, val2 = loc1[i], loc2[j]
-            
-#             min_dist = min(min_dist, abs(val1 - val2))
-            
-#             # LOGIC FIX: Compare the values, not the lists
-#             if val1 < val2:
-#                 i += 1
-#             else:
-#                 j += 1
-        
-#         return min_dist
+        return min_dist
 
 
 # Your WordDistance object will be instantiated and called as such:
