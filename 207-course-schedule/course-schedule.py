2a951c2460fbcc46435c1008d13b2bd091fedcb2
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-01 20:23:30 -0500

    Time: 6 ms (50.14%) | Memory: 21.7 MB (5.15%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,40 +1,35 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj_list = {}
-
-        for src, dst in prerequisites:
-            if src in adj_list:
-                adj_list[src].append(dst)
-            else:
-                adj_list[src] = [dst]
-            if dst not in adj_list:
-                adj_list[dst] = []
-        
-        path = set()
+        adj_list = {i: [] for i in range(numCourses)}
         visited = set()
+        for src, dst in prerequisites:
+            adj_list[src].append(dst)
+
+        path = set()
 
         def dfs(node):
-            
             if node in path:
-                return False
-
-            if node in visited:
                 return True
             
+            if node in visited:
+                return False
+
             path.add(node)
-
+        
             for neighbour in adj_list[node]:
-                if not dfs(neighbour):
-                    return False
-            
+                if dfs(neighbour):
+                    return True
+                
             path.remove(node)
-
+            
             visited.add(node)
 
-            return True
+            return False
 
-        for i in adj_list.keys():
-            if not dfs(i):
+        for i in range(numCourses):
+            if dfs(i):
                 return False
 
         return True
+
+
