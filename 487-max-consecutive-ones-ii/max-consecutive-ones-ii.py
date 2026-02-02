7c7efdbfda33dee33110ba385accbbb63f6c6889
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-01 19:10:06 -0500

    Time: 25 ms (81.95%) | Memory: 19.7 MB (31.51%) - LeetSync

--- 487-max-consecutive-ones-ii/max-consecutive-ones-ii.py
+++ 487-max-consecutive-ones-ii/max-consecutive-ones-ii.py
@@ -5,20 +5,15 @@ class Solution:
         temp_count = 0
         max_count = 0
         for j in range(len(nums)):
-            if nums[j] == 1:
-                temp_count = (j - i + 1)
-            else:
+            if nums[j] == 0:
                 zero_count += 1
-                if zero_count <= 1:
-                    temp_count = (j - i + 1)
-                    max_count = max(max_count, temp_count)
-                else:
-                    while zero_count > 1:
-                        if nums[i] == 0:
-                            zero_count -= 1
-                        i += 1
-                    temp_count = (j - i + 1)
-            max_count = max(max_count, temp_count)
+            
+            while zero_count > 1:
+                if nums[i] == 0:
+                    zero_count -= 1
+                i += 1
+            
+            max_count = max(max_count, j - i + 1)
 
         return max_count
                 
