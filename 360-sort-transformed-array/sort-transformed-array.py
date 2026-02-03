60ddf611e8b435655594c22439e7e1b65732b451
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-02 23:19:22 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (13.30%) - LeetSync

--- 360-sort-transformed-array/sort-transformed-array.py
+++ 360-sort-transformed-array/sort-transformed-array.py
@@ -4,32 +4,38 @@ class Solution:
         result = [0] * n
         i, j = 0, n - 1
         
+        # Consistent logic: if a >= 0, we fill from the end.
         if a >= 0:
-            index_ptr = n-1
+            index_ptr = n - 1
         else:
             index_ptr = 0
 
         while i <= j:
-            temp_i = a *(nums[i] * nums[i]) + b * nums[i] + c
-            temp_j = a * (nums[j] * nums[j]) + b * nums[j] + c
+            # Calculate the transformed values
+            # (In Python, this variable definition inside the loop is perfectly fine)
+            val_i = a * (nums[i] * nums[i]) + b * nums[i] + c
+            val_j = a * (nums[j] * nums[j]) + b * nums[j] + c
 
             if a >= 0:
-                temp_v = max(temp_i, temp_j)
-                result[index_ptr] = temp_v
-                index_ptr -= 1
-                if temp_i > temp_j:
+                # Case 1: 'a' is positive (or 0). The largest values are at the ends.
+                # We want the MAX to go at the end of our result array.
+                if val_i >= val_j:
+                    result[index_ptr] = val_i
                     i += 1
                 else:
-                    j -=1
+                    result[index_ptr] = val_j
+                    j -= 1
+                index_ptr -= 1
                 
             else:
-                temp_v = min(temp_i, temp_j)
-                result[index_ptr] = temp_v
-                index_ptr += 1
-                if temp_i < temp_j:
+                # Case 2: 'a' is negative. The smallest values are at the ends.
+                # We want the MIN to go at the start of our result array.
+                if val_i < val_j:
+                    result[index_ptr] = val_i
                     i += 1
                 else:
-                    j -=1
+                    result[index_ptr] = val_j
+                    j -= 1
+                index_ptr += 1
 
         return result
-                
