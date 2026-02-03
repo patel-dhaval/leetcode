e7647e24d5d7c2ec982182cc8b0bff98896e832f
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-03 12:44:17 -0500

    Time: 183 ms (15.48%) | Memory: 21.1 MB (7.84%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,31 +1,29 @@
+from collections import Counter, deque
+
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        
-        ## Max heap to track the max_counts values up top, since the goal would be to schedule them earlier 
-        max_heap = []
-        heapq.heapify(max_heap)
-        
-        temp = deque()
-        ## Counter gives us a dict of the keys and the counts of the keys
-        val = Counter(tasks)
-    
-        for k, v in val.items():
-            ## Appending the max counts first - so that the heap sorts on it
-            heapq.heappush(max_heap, [-v, k, 0])
-        
         time = 0
-        while max_heap or temp:
+        queue = collections.deque()
+
+        max_heap = []
+
+        task_count = dict(Counter(tasks))
+
+        for k, v in task_count.items():
+            heapq.heappush(max_heap, [-v, k])
+
+        while max_heap or queue:
             time += 1
-            ## Process heap - pick up a task, update its time and add to queue
+            
             if max_heap:
-                v, k, time_passed = heapq.heappop(max_heap)
-                v = abs(v)
-                v -= 1
-                if v:
-                    temp.append([-v, k, time+n])
-            ## Check if vals in queue, check time of the task, and execute the tasks if the time matches
-            if temp and temp[0][2] == time:
-                temp_val = temp.popleft()
-                heapq.heappush(max_heap, temp_val)
-    
+                count, char = heapq.heappop(max_heap)
+                updated_count = count
+                updated_count += 1
+                if updated_count < 0:
+                    queue.append([updated_count,char,time + n])
+
+            if queue and queue[0][2] == time:
+                ready_task = queue.popleft() 
+                heapq.heappush(max_heap, [ready_task[0],ready_task[1]])
+        
         return time
