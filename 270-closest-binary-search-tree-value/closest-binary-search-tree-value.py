51b6d7568aa2d55760f8638f198d9436925ea892
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-06 20:25:40 -0500

    Time: 3 ms (18.10%) | Memory: 21.2 MB (8.08%) - LeetSync

--- 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
@@ -6,24 +6,40 @@
 #         self.right = right
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        diff_stack = []
 
-        stack = []
-        curr = root
-        # stack.append(curr)
-        while stack or curr:
-            if curr:
-                stack.append(curr)
-                curr = curr.left
-            else:
-                curr = stack.pop()
-                diff = abs(target - curr.val)
-                if not diff_stack:
-                    diff_stack.append([diff, curr.val])
-                elif diff_stack[-1][0] > diff:
-                    diff_stack.append([diff, curr.val])
-                curr = curr.right
+        closest = root.val
+        stack = [root]
+        
+        while stack:
+            node = stack.pop()            
+            if abs(target - node.val) < abs(target - closest):
+                closest = node.val
+            elif abs(target - node.val) == abs(target - closest):
+                closest = min(closest, node.val)
+                
+            if node.left: stack.append(node.left)
+            if node.right: stack.append(node.right)
             
-        return diff_stack[-1][1]
+        return closest
+
+
+        # # Good for BT but need to optimize for BST
+        # diff_stack = []
+        # stack = []
+        # curr = root
+        # while stack or curr:
+        #     if curr:
+        #         stack.append(curr)
+        #         curr = curr.left
+        #     else:
+        #         curr = stack.pop()
+        #         diff = abs(target - curr.val)
+        #         if not diff_stack:
+        #             diff_stack.append([diff, curr.val])
+        #         elif diff_stack[-1][0] > diff:
+        #             diff_stack.append([diff, curr.val])
+        #         curr = curr.right
+            
+        # return diff_stack[-1][1]
 
         
