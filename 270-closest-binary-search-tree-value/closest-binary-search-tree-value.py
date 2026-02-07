25cb7799cda9958d7b8148221c35150af0ec026e
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-06 20:20:47 -0500

    Time: 4 ms (8.41%) | Memory: 21.1 MB (26.79%) - LeetSync

---
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        diff_stack = []
+
+        stack = []
+        curr = root
+        stack.append(curr)
+        while stack or curr:
+            if curr:
+                stack.append(curr)
+                curr = curr.left
+            else:
+                curr = stack.pop()
+                diff = abs(target - curr.val)
+                if not diff_stack:
+                    diff_stack.append([diff, curr.val])
+                elif diff_stack[-1][0] > diff:
+                    diff_stack.append([diff, curr.val])
+                curr = curr.right
+            
+        return diff_stack[-1][1]
+
+        
