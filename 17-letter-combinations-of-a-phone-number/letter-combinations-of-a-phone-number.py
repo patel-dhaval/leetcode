2e236a17aae0d5113785c7dbc747015ad87b8525
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-07 19:14:58 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.13%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,37 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        
-        if not digits:  # Handle empty input early
-            return []
-        
-        num_letter_map = {
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w','x', 'y', 'z'],
-        }
+        digit_to_number_map = {2: "abc", 3:"def", 4: "ghi", 5: "jkl", 6: "mno", 7:"pqrs", 8: "tuv", 9: "wxyz"}
+
         res = []
-        def helper(index, currComb):
+
+        def backtrack(idx, char_arr):
+            if idx > len(digits):
+                res.append("".join(char_arr.copy()))
+                return
             
-            if index > len(digits):
+            if len(char_arr) == len(digits):
+                res.append("".join(char_arr.copy()))
                 return
 
-            if index == len(digits):
-                res.append(''.join(currComb.copy()))
-                return
-        
-            for v in num_letter_map[digits[index]]:
-                currComb.append(v)
-                helper(index+1, currComb)
-                currComb.pop()
-        
-        
-        helper(0, [])
-        
+            for j in range(idx, len(digits)):
+                char = digit_to_number_map[int(digits[j])]
+                for c in char:
+                    char_arr.append(c)
+                    backtrack(j+1, char_arr)
+                    char_arr.pop()
+            
+        backtrack(0, [])
         return res
 
             
