87575d147a51231141a657887bb65687518ca741
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-09 18:14:31 -0500

    Time: 47 ms (88.44%) | Memory: 20.4 MB (59.69%) - LeetSync

--- 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.py
+++ 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.py
@@ -2,31 +2,33 @@ from threading import Lock, Condition
 class BoundedBlockingQueue(object):
 
     def __init__(self, capacity: int):
-        self.queue = deque()
         self.capacity = capacity
+        self.queue = collections.deque()
         self.lock = Lock()
         self.not_full = Condition(self.lock)
         self.not_empty = Condition(self.lock)
-        
 
     def enqueue(self, element: int) -> None:
         with self.not_full:
-            while len(self.queue)==self.capacity:
+            while len(self.queue) == self.capacity:
                 self.not_full.wait()
+
             self.queue.append(element)
-            self.not_empty.notify()
-        
+
+            self.not_empty.notify() 
 
     def dequeue(self) -> int:
         with self.not_empty:
-            while not self.queue:
+            while not len(self.queue):
                 self.not_empty.wait()
+
             val = self.queue.popleft()
-            self.not_full.notify()
+
+            self.not_full.notify() 
+
             return val
+
         
 
     def size(self) -> int:
-        with self.lock:
-            return len(self.queue)
-        
+        return len(self.queue)
