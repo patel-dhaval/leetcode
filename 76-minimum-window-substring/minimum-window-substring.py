30cf49358883f25628fdc161ddbd2af7f07f06fc
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-11 10:36:57 -0500

    Time: 59 ms (87.18%) | Memory: 19.8 MB (47.75%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,37 +1,38 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t):
+        if len(t) > len(s):
             return ""
 
-        countT, window = {}, {}
+        countT = {}
+        window = {}
+
         for c in t:
             countT[c] = 1 + countT.get(c, 0)
 
-        have, need = 0, len(countT)
-        res, resLen = [-1, -1], float("infinity")
+        have = 0
+        need = len(countT)
+        res = [-1, -1]
+        resLen = float('inf')
         l = 0
         for r in range(len(s)):
-            c = s[r]
-            window[c] = 1 + window.get(c, 0)
+            curr_char = s[r]
+            window[curr_char] = 1 + window.get(curr_char, 0)
 
-            if c in countT and window[c] == countT[c]:
+            if curr_char in countT and window[curr_char] == countT[curr_char]:
                 have += 1
-
+            
             while have == need:
-                # update our result
-                if (r - l + 1) < resLen:
-                    res = [l, r]
+                if r - l + 1 < resLen:
                     resLen = r - l + 1
-                # pop from the left of our window
+                    res = [l, r]
+                
                 window[s[l]] -= 1
+
                 if s[l] in countT and window[s[l]] < countT[s[l]]:
                     have -= 1
+                
                 l += 1
-        l, r = res
-        return s[l : r + 1] if resLen != float("infinity") else ""
-
-
-
-
-
         
+        l, r = res
+
+        return s[l:r+1] if resLen != float('inf') else ""
