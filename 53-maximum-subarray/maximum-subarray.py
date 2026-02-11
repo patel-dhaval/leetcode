b74bbfeb739f12a36fd02bcf26d35c0bc4e5b2e5
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-11 10:10:21 -0500

    Time: 37 ms (65.64%) | Memory: 31.6 MB (34.54%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,11 +1,16 @@
+"""
+DRY RUN:
+nums = [-2, 1, -3, 4,-1,2,1,-5]
+curr_sum = 1
+max_sum = 6
+"""
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = nums[0]
         curr_sum = 0
-        for r in range(len(nums)):
+        max_sum = nums[0]
+        for n in nums:
             curr_sum = max(curr_sum, 0)
-            curr_sum += nums[r]
-            max_sum = max(curr_sum, max_sum)
+            curr_sum += n
+            max_sum = max(max_sum, curr_sum)
         
         return max_sum
-            
