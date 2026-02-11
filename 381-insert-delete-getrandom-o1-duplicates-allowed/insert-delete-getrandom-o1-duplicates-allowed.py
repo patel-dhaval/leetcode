dd5add75f200c795f9975173d57a9a81de684dba
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-11 10:26:26 -0500

    Time: 103 ms (77.50%) | Memory: 68 MB (68.18%) - LeetSync

--- 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -1,39 +1,48 @@
+"""
+DRY RUN:
+["RandomizedCollection", "insert", "insert", "insert", "getRandom", "remove", "getRandom"]
+[[], [1], [1], [2], [], [1], []]
+
+vals = 2
+indices = 2:0
+
+idx_to_remove = 0
+val_to_swap = 2
+vals[0] = 2
+self.indices[2] = 0
+"""
 class RandomizedCollection:
 
     def __init__(self):
-        self.indices = DefaultDict(set)
         self.vals = []
+        self.indices = collections.defaultdict(set)
 
     def insert(self, val: int) -> bool:
         isNew = False
-        if not self.indices[val]: # Returns True if key missing OR set is empty
+        if not val in self.indices:
             isNew = True
-
+        
         self.vals.append(val)
         self.indices[val].add(len(self.vals) - 1)
         return isNew
 
     def remove(self, val: int) -> bool:
-        
-        if len(self.indices[val]) == 0:
+        if val not in self.indices:
             return False
-
+        
         idx_to_remove = self.indices[val].pop()
         val_to_swap = self.vals[-1]
-        
-        self.indices[val_to_swap].add(idx_to_remove)
-        self.indices[val_to_swap].remove(len(self.vals)-1)
-        
         self.vals[idx_to_remove] = self.vals[-1]
-        self.vals.pop()
-        
-        if not self.indices[val]:
-            del self.indices[val]
+        self.indices[val_to_swap].add(idx_to_remove)
+        self.indices[self.vals[-1]].remove(len(self.vals)-1)
 
+        if len(self.indices[val]) == 0:
+            del self.indices[val] 
+        self.vals.pop()
         return True
 
     def getRandom(self) -> int:
-        return self.vals[randint(0, len(self.vals) - 1)]
+        return self.vals[randint(0, len(self.vals)-1)]
 
 
 # Your RandomizedCollection object will be instantiated and called as such:
