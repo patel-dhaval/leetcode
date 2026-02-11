9fbcfe701c6c30eb969438a260671ad8bdf08745
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-10 22:51:58 -0500

    Time: 0 ms (100.00%) | Memory: 21.2 MB (20.25%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,29 +1,38 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        indegree = [0] * numCourses
-        adj_lst = {i: [] for i in range(numCourses) }
-        queue = collections.deque()
         toposort = []
         visited = set()
-        for src, dst in prerequisites:
-            adj_lst[src].append(dst)
-            indegree[dst] += 1
-        
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
+        path = set()
 
-        while queue:
-            node = queue.popleft()
-            visited.add(node)
-            for nei in adj_lst[node]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    queue.append(nei)
+
+        adj_list = [[] for _ in range(numCourses)]
+
+        for src, dst in prerequisites:
+            adj_list[src].append(dst)
+
         
+        def dfs(node):
+            if node in path:
+                return False
+            
+            if node in visited:
+                return True
+            
+            path.add(node)
+
+            for neighbour in adj_list[node]:
+                if not dfs(neighbour):
+                    return False
+            
+            path.remove(node)
+            visited.add(node)
             toposort.append(node)
         
-        if len(toposort) != numCourses:
-            return []
+            return True
         
-        return toposort[::-1]
+        for course in range(numCourses):
+            if course not in visited:
+                if not dfs(course):
+                    return []
+        
+        return toposort
