6be0922f2a2525fabb88075658e27d03d70a4e6c
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-17 21:27:42 -0500

    Time: 7 ms (88.02%) | Memory: 26.2 MB (36.31%) - LeetSync

--- 244-shortest-word-distance-ii/shortest-word-distance-ii.py
+++ 244-shortest-word-distance-ii/shortest-word-distance-ii.py
@@ -1,30 +1,51 @@
-from collections import defaultdict
+"""
+DRY RUN:
+["WordDistance", "shortest", "shortest"]
+[[["practice", "makes", "perfect", "coding", "makes"]], ["coding", "practice"], ["makes", "coding"]]
 
+self.locations {practice: [0], makes: [1,4], coding: [3]}
+
+loc1 = [1,4]
+loc2 = [3]
+
+i = 1
+j = 1
+
+dist1 = 4
+ 
+dist2 = 3
+
+ans = 1
+
+"""
 class WordDistance:
 
     def __init__(self, wordsDict: List[str]):
-        self.locations = defaultdict(list)
-        for i, word in enumerate(wordsDict):
-            self.locations[word].append(i)
+        self.locations = collections.defaultdict(list)
+
+        for idx, word in enumerate(wordsDict):
+            self.locations[word].append(idx)
+        
 
     def shortest(self, word1: str, word2: str) -> int:
         loc1 = self.locations[word1]
         loc2 = self.locations[word2]
-        
+
         i, j = 0, 0
-        min_dist = float("inf")
+        ans = float('inf')
 
         while i < len(loc1) and j < len(loc2):
-            val1, val2 = loc1[i], loc2[j]
-            
-            min_dist = min(min_dist, abs(val1 - val2))
-            
-            if val1 < val2:
-                i += 1
-            else:
+            dist1 = loc1[i]
+            dist2 = loc2[j]
+
+            ans = min(ans, abs(dist1 - dist2))
+
+            if dist1 > dist2:
                 j += 1
-        
-        return min_dist
+            else:
+                i += 1
+
+        return ans
 
 
 # Your WordDistance object will be instantiated and called as such:
