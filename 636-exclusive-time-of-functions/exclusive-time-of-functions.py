d80a1a706a9b6623f5ee413cb5beb61ec719bbc3
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-18 20:33:46 -0500

    Time: 7 ms (63.44%) | Memory: 19.5 MB (17.37%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.py
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.py
@@ -1,22 +1,30 @@
+"""
+DRY RUN:
+n = 2, logs = ["0:start:0","1:start:2","1:end:5","0:end:6"]
+
+res = [,4]
+stack = [0]
+prev_time = 5
+"""
+
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
-        result = [0] * n
+        res = [0] * n
         prev_time = 0
         stack = []
         for log in logs:
-            f_id, function, timestamp = log.split(":")
-            f_id = int(f_id)
-            timestamp = int(timestamp)
+            idx, function, time_stamp = log.split(":")
+            idx = int(idx)
+            timestamp = int(time_stamp)
             if function == "start":
                 if stack:
-                    result[stack[-1]] += timestamp - prev_time
-                
-                stack.append(f_id)
+                    curr_idx = stack[-1]
+                    res[curr_idx] += timestamp - prev_time
+                stack.append(idx)
                 prev_time = timestamp
-
             else:
-                result[stack.pop()] += timestamp - prev_time + 1
+                curr_idx = stack.pop()
+                res[curr_idx] += timestamp - prev_time + 1
                 prev_time = timestamp + 1
         
-        return result
-
+        return res
