b2d9ecc6853c5a1112d3e59251aaaf866273e08a
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-19 23:02:56 -0500

    Time: 3 ms (31.54%) | Memory: 19.4 MB (53.46%) - LeetSync

--- 360-sort-transformed-array/sort-transformed-array.py
+++ 360-sort-transformed-array/sort-transformed-array.py
@@ -1,43 +1,52 @@
 """
-Dry Run:
-nums = [-4,-2,2,4], a = 1, b = 3, c = 5
-index_ptr = 3,2,1,0
-val_l = 3
-val_r = 3
-res = [3,9,15,33]
-"""
+DRY RUN:
 
+nums = [-4,-2,2,4], a = 1, b = 3, c = 5
+
+res = [3,9,15,33]
+index_ptr = 3
+l = 1
+r = 1
+val_1 = 9
+val_2 = 33
+
+"""
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
-            n = len(nums)
-            res = [0] * n
+        """
+        since the equation is a parabola, the a value here would control the direction, and the max value would lie on one of the extremes.
+        If a is positive, then the max value would be present on the right extreme, else it would be on the left extreme
+        """
+
+        res = [0] * len(nums)
+        
+        if a>=0:
+            index_ptr = len(nums) - 1
+        else:
             index_ptr = 0
 
-            if a >= 0:
-                index_ptr = len(nums) - 1
-            
-            l = 0
-            r = len(nums) - 1
+        l = 0
+        r = len(nums) -1
 
-            while l <= r:
-                val_l = a * (nums[l] * nums[l]) + b * nums[l] + c
-                val_r = a * (nums[r] * nums[r]) + b * nums[r] + c
+        while l <= r:
+            val_1 = a * (nums[l]**2) + b * nums[l] + c
+            val_2 = a * (nums[r]**2) + b * nums[r] + c
 
-                if a>=0:
-                    if val_l > val_r:
-                        res[index_ptr] = val_l
-                        l+=1
-                    else:
-                        res[index_ptr] = val_r
-                        r-=1
-                    index_ptr -= 1
+            if a>=0:
+                if val_1 > val_2:
+                    res[index_ptr] = val_1
+                    l += 1
                 else:
-                    if val_l > val_r:
-                        res[index_ptr] = val_r
-                        r-=1
-                    else:
-                        res[index_ptr] = val_l
-                        l+=1
-                    index_ptr += 1
-            
-            return res
+                    res[index_ptr] = val_2
+                    r -= 1
+                index_ptr -= 1
+            else:
+                if val_1 < val_2:
+                    res[index_ptr] = val_1
+                    l += 1
+                else:
+                    res[index_ptr] = val_2
+                    r -= 1
+                index_ptr += 1
+        
+        return res
