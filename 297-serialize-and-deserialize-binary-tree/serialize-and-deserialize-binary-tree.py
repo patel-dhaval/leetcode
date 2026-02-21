0b836bad969ad192c6fc621c9230387a1a711457
Dhaval Patel <dhavaljp.95@gmail.com>
2026-02-21 17:54:48 -0500

    Time: 93 ms (35.03%) | Memory: 22.6 MB (59.78%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -13,19 +13,23 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        res = []
+        if not root:
+            return "N"
 
-        def dfs(node):
-            if not node:
+        res = []
+        queue = collections.deque()
+
+        queue.append(root)
+
+        while queue:
+            curr = queue.popleft()
+            if curr:
+                res.append(str(curr.val))
+                queue.append(curr.left)
+                queue.append(curr.right)
+            else:
                 res.append("N")
-                return
-            val = str(node.val)
-            res.append(val)
-            dfs(node.left)
-            dfs(node.right)
-        
-        dfs(root)
-        
+    
         return ",".join(res)
 
     def deserialize(self, data):
@@ -34,21 +38,28 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        vals = data.split(",")
-        self.i = 0
-        def dfs():
-            if vals[self.i] == "N":
-                self.i += 1
-                return None
-            
-            node = TreeNode(int(vals[self.i]))
-            self.i += 1
-            node.left = dfs()
-            node.right = dfs()
-            return node
-
-        return dfs()
+        if not data or data == "N":
+            return None
         
+        # If serialized as a string, split it back into a list
+        data = data.split(",") if isinstance(data, str) else data
+        root = TreeNode(int(data[0]))
+        queue = collections.deque()
+        queue.append(root)
+        idx = 1
+        while queue and (idx) < len(data):
+            curr = queue.popleft()
+            if data[idx] != "N":
+                curr.left = TreeNode(int(data[(idx)]))
+                queue.append(curr.left)
+            idx+=1
+
+            if data[idx] != "N":
+                curr.right = TreeNode(int(data[idx]))
+                queue.append(curr.right)
+            idx+=1
+
+        return root
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
