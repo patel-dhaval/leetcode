0b726c9db19cc3ce3c84695bd60746da19e71088
Dhaval Patel <dhavaljp.95@gmail.com>
2026-03-02 12:11:44 -0500

    Time: 173 ms (31.31%) | Memory: 78.7 MB (94.87%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,24 +1,21 @@
 class Node:
-    def __init__(self, key, val):
+    def __init__(self, key, value):
         self.key = key
-        self.val = val
+        self.value = value
         self.freq = 1
-        self.next, self.prev = None, None
-
+        self.prev, self.next = None, None
 
 class DLinkedList:
     def __init__(self):
-        self.head, self.tail = Node(0,0), Node(0,0)
-        self.head.next, self.tail.prev = self.tail, self.head
         self.size = 0
+        self.head, self.tail = Node(0,0), Node(0, 0)
+        self.head.next, self.tail.prev = self.tail, self.head
 
-    
-    def add(self, new_node):
+    def add(self, node):
+        # tail.prev -> <- node -> <- tail
         prev, nxt = self.tail.prev, self.tail
-        new_node.next = nxt
-        new_node.prev = prev
-        nxt.prev = new_node
-        prev.next = new_node
+        node.next, node.prev = nxt, prev
+        prev.next, nxt.prev = node, node
         self.size += 1
     
     def remove(self, node):
@@ -27,57 +24,62 @@ class DLinkedList:
         self.size -= 1
     
     def remove_lru(self):
-        if self.size == 0: return None
-        lru_node = self.head.next
-        self.remove(lru_node)
-        return lru_node
+        if self.size > 0:
+            node = self.head.next
+            self.remove(node)
+            return node
+        else:
+            return None
 
 class LFUCache:
 
     def __init__(self, capacity: int):
+        self.capacity = capacity
         self.key_map = {}
         self.freq_map = collections.defaultdict(DLinkedList)
         self.min_freq = 0
-        self.capacity = capacity
-    
-    def _update_freq(self, node):
+        
+    def update(self, node):
         old_freq = node.freq
-        self.freq_map[old_freq].remove(node)
-
-        if old_freq == self.min_freq and self.freq_map[old_freq].size == 0:
-            self.min_freq += 1
+        if old_freq in self.freq_map:
+            self.freq_map[old_freq].remove(node)
         
         node.freq += 1
-
         self.freq_map[node.freq].add(node)
 
+        if self.freq_map[old_freq].size == 0:
+            del self.freq_map[old_freq]
+            if self.min_freq == old_freq:
+                self.min_freq = old_freq + 1
 
     def get(self, key: int) -> int:
-        if key not in self.key_map: return -1
+        if key not in self.key_map:
+            return -1
 
         node = self.key_map[key]
-        self._update_freq(node)
-        return node.val
+        self.update(node)
+        return node.value
 
-    def put(self, key: int, value: int) -> None:
-        if self.capacity == 0: return
-
-        if key in self.key_map:
-            node = self.key_map[key]
-            node.val = value
-            self._update_freq(node)
-            return
-        
-        if len(self.key_map) == self.capacity:
-            lru_node = self.freq_map[self.min_freq].remove_lru()
-            del self.key_map[lru_node.key]
-            
-        new_node = Node(key, value)
-        self.key_map[key] = new_node
-        self.freq_map[1].add(new_node)
-        self.min_freq = 1
-        
     
+    def put(self, key: int, value: int) -> None:
+        if self.capacity <= 0:
+            return False
+
+        if key not in self.key_map:
+            if self.capacity <= len(self.key_map):
+                node = self.freq_map[self.min_freq].remove_lru()
+                if node:
+                    del self.key_map[node.key]
+
+            self.min_freq = 1
+            node = Node(key, value)
+            self.key_map[key] = node
+            self.freq_map[self.min_freq].add(node)
+        else:
+            node = self.key_map[key]
+            node.value = value
+            self.update(node)
+
 
 # Your LFUCache object will be instantiated and called as such:
 # obj = LFUCache(capacity)
