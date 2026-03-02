2c7eae560106af68bed74206238da37a0fce22b8
Dhaval Patel <dhavaljp.95@gmail.com>
2026-03-02 15:04:47 -0500

    Time: 106 ms (67.32%) | Memory: 68.1 MB (51.06%) - LeetSync

--- 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -1,45 +1,39 @@
-"""
-DRY RUN:
-["RandomizedCollection", "insert", "insert", "insert", "getRandom", "remove", "getRandom"]
-[[], [1], [1], [2], [], [1], []]
-
-vals = 2
-indices = 2:0
-
-idx_to_remove = 0
-val_to_swap = 2
-vals[0] = 2
-self.indices[2] = 0
-"""
 class RandomizedCollection:
 
     def __init__(self):
-        self.vals = []
         self.indices = collections.defaultdict(set)
+        self.vals = []
 
     def insert(self, val: int) -> bool:
         isNew = False
-        if not val in self.indices:
+
+        if val not in self.indices:
             isNew = True
-        
+
         self.vals.append(val)
-        self.indices[val].add(len(self.vals) - 1)
+        self.indices[val].add(len(self.vals)-1)
+
         return isNew
 
     def remove(self, val: int) -> bool:
+        
         if val not in self.indices:
             return False
-        
+
         idx_to_remove = self.indices[val].pop()
         val_to_swap = self.vals[-1]
-        self.vals[idx_to_remove] = self.vals[-1]
+
+        self.vals[idx_to_remove] = val_to_swap
         self.indices[val_to_swap].add(idx_to_remove)
-        self.indices[self.vals[-1]].remove(len(self.vals)-1)
+        self.indices[val_to_swap].remove(len(self.vals)-1)
 
         if len(self.indices[val]) == 0:
-            del self.indices[val] 
+            del self.indices[val]
+        
         self.vals.pop()
+
         return True
+
 
     def getRandom(self) -> int:
         return self.vals[randint(0, len(self.vals)-1)]
