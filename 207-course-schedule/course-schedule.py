1ecc950009cbed1bf6f3836458b518d6e1236625
Dhaval Patel <dhavaljp.95@gmail.com>
2026-03-01 22:50:58 -0500

    Time: 7 ms (46.20%) | Memory: 20.5 MB (75.22%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,35 +1,31 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj_list = {i: [] for i in range(numCourses)}
+        adj_lst = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        queue = collections.deque()
+        toposort = []
         visited = set()
         for src, dst in prerequisites:
-            adj_list[src].append(dst)
+            adj_lst[src].append(dst)
+            indegree[dst] += 1
+        
 
-        path = set()
-
-        def dfs(node):
-            if node in path:
-                return True
-            
-            if node in visited:
-                return False
-
-            path.add(node)
-            visited.add(node)
-            for neighbour in adj_list[node]:
-                if dfs(neighbour):
-                    return True
-                
-            path.remove(node)
-            
+        for idx in range(numCourses):
+            if indegree[idx] == 0:
+                queue.append(idx)
 
 
+        while queue:
+            curr = queue.popleft()
+            visited.add(curr)
+            for neighbour in adj_lst[curr]:
+                if neighbour not in visited:
+                    indegree[neighbour] -= 1
+                    if indegree[neighbour] == 0:
+                        queue.append(neighbour)
+            toposort.append(curr)
+        
+        if len(toposort) != numCourses:
             return False
-
-        for i in range(numCourses):
-            if dfs(i):
-                return False
-
+        
         return True
-
-
