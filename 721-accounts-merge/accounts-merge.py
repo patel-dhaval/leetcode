d7ce1980964302503cb28e71ad79d9228c98e2ad
Dhaval Patel <dhavaljp.95@gmail.com>
2026-03-11 12:39:47 -0400

    Time: 23 ms (79.49%) | Memory: 22.3 MB (76.52%) - LeetSync

--- 721-accounts-merge/accounts-merge.py
+++ 721-accounts-merge/accounts-merge.py
@@ -1,24 +1,27 @@
+import collections
+from typing import List
+
 class DSU:
-    def __init__(self, n):
-        self.par = {}
-        self.rank = {}
-
-        for i in range(n):
-            self.par[i] = i
-            self.rank[i] = 1
+    def __init__(self, n: int):
+        # Using lists is faster than dictionaries for 0 to n-1 indices
+        self.par = list(range(n)) 
+        self.rank = [1] * n
     
-    def find(self, node):
-        p = self.par[node]
+    def find(self, node: int) -> int:
+        # Standard iterative path compression
+        root = node
+        while root != self.par[root]:
+            # Path halving: point to grandparent
+            self.par[root] = self.par[self.par[root]] 
+            root = self.par[root]
+        return root
 
-        while p != self.par[p]:
-            self.par[p] = self.par[self.par[p]]
-            p = self.par[p]
-        
-        return p
-
-    def union(self, u, v):
+    def union(self, u: int, v: int):
         pu = self.find(u)
         pv = self.find(v)
+
+        if pu == pv:
+            return 
 
         if self.rank[pu] > self.rank[pv]:
             self.par[pv] = pu
@@ -31,23 +34,25 @@ class DSU:
 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        emails_to_account = collections.defaultdict(str)
-        
         dsu = DSU(len(accounts))
+        email_to_account_idx = {} 
 
+        # 1. Map emails to account indices and union overlapping ones
         for idx, account in enumerate(accounts):
             for email in account[1:]:
-                if email in emails_to_account:
-                    dsu.union(emails_to_account[email], idx)
+                if email in email_to_account_idx:
+                    dsu.union(email_to_account_idx[email], idx)
                 else:
-                    emails_to_account[email] = idx
+                    email_to_account_idx[email] = idx
         
-        res = collections.defaultdict(list)
+        # 2. Group emails by their root account index
+        components = collections.defaultdict(list)
+        for email, account_idx in email_to_account_idx.items():
+            root_idx = dsu.find(account_idx)
+            components[root_idx].append(email)
 
-        for email, account in emails_to_account.items():
-            res[dsu.find(account)].append(email)
-
-        
-        return [[accounts[i][0]] + sorted(emails) for i, emails in res.items()]
-
-        
+        # 3. Format the result
+        return [
+            [accounts[root_idx][0]] + sorted(emails) 
+            for root_idx, emails in components.items()
+        ]
