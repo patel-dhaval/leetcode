ba4c432592ade2739743b217794abb57aa0fe3fd
Dhaval Patel <dhavaljp.95@gmail.com>
2026-03-12 16:55:56 -0400

    Time: 2 ms (81.33%) | Memory: 19.1 MB (98.82%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,43 +1,43 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
         fresh_oranges = 0
         rotten_oranges = 0
-        ROWS = len(grid)
-        COLS = len(grid[0])
+        directions = [[1,0], [-1, 0], [0,1], [0,-1]]
         queue = collections.deque()
 
-        for r in range(ROWS):
-            for c in range(COLS):
+
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == 1:
                     fresh_oranges += 1
                 elif grid[r][c] == 2:
-                    queue.append((r,c))
                     rotten_oranges += 1
-
+                    queue.append((r,c))
+        
         if fresh_oranges == 0:
             return 0
 
         if rotten_oranges == 0:
             return -1
-
-        neighbours = [[1,0], [-1, 0], [0, 1], [0, -1]] 
-        time = 0
+        time = 0        
         while queue:
             for _ in range(len(queue)):
-                r, c = queue.popleft()
-
-                for dr, dc in neighbours:
-                    row = r + dr
-                    col = c + dc
-
-                    if min(row, col) < 0 or row == ROWS or col == COLS or grid[row][col] == 0 or grid[row][col] == 2:
+                r, c  =  queue.popleft()
+                for dr, dc in directions:
+                    nr = r + dr
+                    nc = c + dc
+                    if min(nr, nc) < 0 or nr == rows or nc == cols or grid[nr][nc] == 0 or grid[nr][nc] == 2:
                         continue
-                    
-                    grid[row][col] = 2
-                    queue.append((row, col))
+
+                    grid[nr][nc] = 2
                     fresh_oranges -= 1
-            
+                    queue.append((nr, nc))
+                    if fresh_oranges == 0:
+                        return time + 1
             time += 1
-            if fresh_oranges == 0:
-                return time
-        return -1
+        
+        return time if fresh_oranges == 0 else -1
+
+
