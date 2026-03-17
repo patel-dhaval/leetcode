823593bfe9a4dd74f3561712c4f48752eb418acd
Dhaval Patel <dhavaljp.95@gmail.com>
2026-03-17 12:25:51 -0400

    Time: 7 ms (49.52%) | Memory: 20.3 MB (89.07%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,30 +1,48 @@
+class DSU:
+    def __init__(self, n):
+        self.par = {}
+        self.rank = {}
+        self.components = n
+        for idx in range(n):
+            self.par[idx] = idx
+            self.rank[idx] = 1
+
+    def find(self, node):
+        root = self.par[node]
+
+        while root != self.par[root]:
+            self.par[root] = self.par[self.par[root]]
+            root = self.par[root]
+
+        return root
+
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+        
+        if pu == pv:
+            return False
+
+        self.components -= 1
+
+        if self.rank[pu] > self.rank[pv]:
+            self.par[pv] = pu
+        elif self.rank[pu] < self.rank[pv]:
+            self.par[pu] = pv
+        else:
+            self.par[pu] = pv
+            self.rank[pv] += 1
+        
+        return True
+
+    def comps(self):
+        return self.components
+
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        visited = set()
+        dsu = DSU(n)
 
-        adj_list = {i: [] for i in range(n)}
+        for u,v in edges:
+            dsu.union(u,v)
 
-        for src, dst in edges:
-            adj_list[src].append(dst)
-            adj_list[dst].append(src)
-
-
-        def dfs(node):
-            if node in visited:
-                return 
-            
-            visited.add(node)
-
-            for neighbour in adj_list[node]:
-                if neighbour not in visited:
-                    dfs(neighbour)
-            
-            return
-        
-        component_count = 0
-        for i in range(n):
-            if i not in visited:
-                dfs(i)
-                component_count += 1
-        
-        return component_count
+        return dsu.comps()
