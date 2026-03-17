45092940178301819a0a0e41a8bd290c2589865b
Dhaval Patel <dhavaljp.95@gmail.com>
2026-03-17 12:16:03 -0400

    Time: 263 ms (26.67%) | Memory: 31.3 MB (7.00%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,30 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        neighbours = [[1,0], [-1, 0], [0,1], [0,-1]]
-        visited = set()
-        island_count = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        directions = [[1,0], [-1, 0], [0,1], [0,-1]]
+        number_of_islands = 0
 
         def dfs(r,c):
-            if (r,c) in visited:
-                return
-
             visited.add((r,c))
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
 
-            for dr, dc in neighbours:
-                row = r + dr
-                col = c + dc
-
-                if min(row,col) < 0 or row == ROWS or col == COLS or (row, col) in visited or grid[row][col] == '0':
+                if min(nr, nc) < 0 or nr == rows or nc == cols or grid[nr][nc] == '0' or (nr, nc) in visited:
                     continue
-                dfs(row,col)
-
+                
+                dfs(nr, nc)
         
-        for r in range(ROWS):
-            for c in range(COLS):
+        visited = set()
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == '1' and (r,c) not in visited:
-                    dfs(r,c)
-                    island_count += 1
-        
-        return island_count
+                    dfs(r, c)
+                    number_of_islands += 1
+
+        return number_of_islands
