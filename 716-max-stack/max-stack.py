e8346ee11482855dd8caa03079b45e90d677b00e
Dhaval Patel <dhavaljp.95@gmail.com>
2026-03-26 10:18:12 -0400

    Time: 162 ms (46.27%) | Memory: 67.8 MB (60.16%) - LeetSync

--- 716-max-stack/max-stack.py
+++ 716-max-stack/max-stack.py
@@ -1,44 +1,42 @@
 class MaxStack:
 
     def __init__(self):
-        self.heap = []
-        self.stack = []
-        self.cnt = 0
-        self.deleted_nodes = set()
-        heapq.heapify(self.heap)
+       self.stack = []
+       self.heap = []
+       self.cnt = 0
+       self.deleted_ids = set()
+       heapq.heapify(self.heap) 
 
     def push(self, x: int) -> None:
         self.stack.append((x, self.cnt))
         heapq.heappush(self.heap, (-x, -self.cnt))
+
         self.cnt += 1
 
     def pop(self) -> int:
-        while self.stack and self.stack[-1][1] in self.deleted_nodes:
+        while self.stack and self.stack[-1][1] in self.deleted_ids:
             self.stack.pop()
-
-        val, key = self.stack.pop()
-        self.deleted_nodes.add(key)
-
+        val, cnt = self.stack.pop()
+        self.deleted_ids.add(cnt)
         return val
 
     def top(self) -> int:
-        while self.stack and self.stack[-1][1] in self.deleted_nodes:
+        while self.stack and self.stack[-1][1] in self.deleted_ids:
             self.stack.pop()
-
         return self.stack[-1][0]
 
     def peekMax(self) -> int:
-        while self.heap and -self.heap[0][1] in self.deleted_nodes:
+        while self.heap and -self.heap[0][1] in self.deleted_ids:
             heapq.heappop(self.heap)
 
         return -self.heap[0][0]
 
     def popMax(self) -> int:
-        while self.heap and -self.heap[0][1] in self.deleted_nodes:
+        while self.heap and -self.heap[0][1] in self.deleted_ids:
             heapq.heappop(self.heap)
 
-        val, key = heapq.heappop(self.heap)
-        self.deleted_nodes.add(-key)
+        val, cnt = heapq.heappop(self.heap)
+        self.deleted_ids.add(-cnt)
         return -val
 
 
