4ad6527774ef8ee4cec7562af70b67933c0b5546
Dhaval Patel <dhavaljp.95@gmail.com>
2026-04-02 10:54:35 -0400

    Time: 51 ms (87.44%) | Memory: 34.6 MB (68.91%) - LeetSync

--- 432-all-oone-data-structure/all-oone-data-structure.py
+++ 432-all-oone-data-structure/all-oone-data-structure.py
@@ -3,75 +3,80 @@ class Node:
         self.count = count
         self.keys = set()
         self.prev, self.next = None, None
-
+    
 class AllOne:
-
     def __init__(self):
         self.key_map = {}
-        self.head, self.tail = Node(0), Node(0)
+        self.head = Node(0)
+        self.tail = Node(0)
         self.head.next, self.tail.prev = self.tail, self.head
 
-    def _add_node_after(self, prev_node, next_count, key):
-        # prev_node -> < - new_node -> <- prev_node.next
-        new_node = Node(next_count)
-        new_node.keys.add(key)
-        prev, nxt = prev_node, prev_node.next
-        new_node.prev, new_node.next = prev, nxt
-        prev.next, nxt.prev = new_node, new_node
 
-        return new_node
-
-
-    def _remove_node_if_empty(self, node):
+    def _add_node(self, prev_node, count):
+        node = Node(count)
+        # prev_node -> <- new_node -> <- prev_node.next
+        prev, nxt = prev_node, prev_node.next 
+        node.prev, node.next = prev, nxt
+        prev.next, nxt.prev = node, node
+        return node
+        
+    
+    def _remove_node(self, node):
         if not node.keys:
             prev, nxt = node.prev, node.next
             prev.next, nxt.prev = nxt, prev
             return True
-
+        
         return False
-
+        
 
     def inc(self, key: str) -> None:
-        curr_node = self.head
+        initial_node = self.head
         if key in self.key_map:
-            curr_node = self.key_map[key]
-            curr_node.keys.remove(key)
-        curr_count = curr_node.count
-        next_count = curr_count + 1
-        if next_count == curr_node.next.count:
-            curr_node.next.keys.add(key)
-            self.key_map[key] = curr_node.next
-        else:
-            new_node = self._add_node_after(curr_node, next_count, key)
-            self.key_map[key] = new_node
+            initial_node = self.key_map[key]
+            initial_node.keys.remove(key)
+            
+        initial_count = initial_node.count
+        next_count = initial_count + 1
         
-        if not curr_node.keys and curr_node != self.head:
-            self._remove_node_if_empty(curr_node)
+        if next_count == initial_node.next.count:
+            self.key_map[key] = initial_node.next
+            initial_node.next.keys.add(key)
+        else:
+            node = self._add_node(initial_node, next_count)
+            node.keys.add(key)
+            self.key_map[key] = node
 
+        if not initial_node.keys and initial_node != self.head:
+            self._remove_node(initial_node)
+
+        return
 
     def dec(self, key: str) -> None:
         if key not in self.key_map:
             return 
+        
+        initial_node = self.key_map[key] 
+        initial_node.keys.remove(key)
 
-        curr_node = self.key_map[key]
-        curr_node.keys.remove(key)
-
-        curr_count = curr_node.count
-        next_count = curr_count - 1
+        initial_count = initial_node.count
+        next_count = initial_count - 1
 
         if next_count == 0:
             del self.key_map[key]
-
-        elif next_count == curr_node.prev.count:
-            curr_node.prev.keys.add(key)
-            self.key_map[key] = curr_node.prev
+        
+        elif next_count == initial_node.prev.count:
+            self.key_map[key] = initial_node.prev
+            initial_node.prev.keys.add(key)
         else:
-            new_node = self._add_node_after(curr_node.prev, next_count, key)
-            self.key_map[key] = new_node
-        
-        
-        if not curr_node.keys and curr_node != self.head:
-            self._remove_node_if_empty(curr_node)
+            node = self._add_node(initial_node.prev, next_count)
+            node.keys.add(key)
+            self.key_map[key] = node
+           
+        if not initial_node.keys and initial_node != self.head:
+            self._remove_node(initial_node)
+
+        return
 
     def getMaxKey(self) -> str:
         if self.tail.prev == self.head: return ""
@@ -80,6 +85,8 @@ class AllOne:
     def getMinKey(self) -> str:
         if self.head.next == self.tail: return ""
         return next(iter(self.head.next.keys))
+
+        
 
 
 # Your AllOne object will be instantiated and called as such:
