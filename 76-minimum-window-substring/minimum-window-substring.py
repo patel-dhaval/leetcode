4d6377e4fe047e5e5de9a1ae7eb4e94cd1714c47
Dhaval Patel <dhavaljp.95@gmail.com>
2026-04-05 12:41:17 -0400

    Time: 59 ms (86.07%) | Memory: 19.7 MB (54.26%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,38 +1,56 @@
+"""
+DRY RUN:
+
+IP: "ABAACDABC"
+
+OP: "ABC"
+countT: {A: 1, B: 1, C: 1}
+
+have = 3
+need = 3
+window = {A: 1, B:1, C:1, D: 0}
+l = 6
+r = 8
+
+curr = C
+
+resLen = 3
+res = [6, 8]
+"""
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(t) > len(s):
             return ""
 
-        countT = {}
-        window = {}
-
+        countT = collections.defaultdict(int)
+        
         for c in t:
-            countT[c] = 1 + countT.get(c, 0)
-
+            countT[c] += 1
+        
         have = 0
         need = len(countT)
-        res = [-1, -1]
-        resLen = float('inf')
+        window = collections.defaultdict(int)
         l = 0
+        res = [-1, -1]
+        resLen = float("inf")
         for r in range(len(s)):
-            curr_char = s[r]
-            window[curr_char] = 1 + window.get(curr_char, 0)
+            curr = s[r]
+            window[curr] += 1
 
-            if curr_char in countT and window[curr_char] == countT[curr_char]:
+            if curr in countT and window[curr] == countT[curr]:
                 have += 1
-            
-            while have == need:
+
+            while have == need:    
                 if r - l + 1 < resLen:
                     resLen = r - l + 1
                     res = [l, r]
-                
-                window[s[l]] -= 1
 
+                window[s[l]] -= 1
                 if s[l] in countT and window[s[l]] < countT[s[l]]:
                     have -= 1
                 
                 l += 1
-        
+            
         l, r = res
 
-        return s[l:r+1] if resLen != float('inf') else ""
+        return s[l: r+1] if resLen != float("inf") else ""
