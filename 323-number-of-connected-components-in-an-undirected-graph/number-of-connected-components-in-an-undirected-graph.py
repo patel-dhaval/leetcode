b891803c72141be37f6a8a55d426d0d4b6c1e508
Dhaval Patel <dhavaljp.95@gmail.com>
2026-04-06 11:48:02 -0400

    Time: 7 ms (49.70%) | Memory: 20.3 MB (83.79%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,48 +1,46 @@
 class DSU:
     def __init__(self, n):
-        self.par = {}
-        self.rank = {}
+        self.par = [[] for _ in range(n)]
+        self.rank = [[] for _ in range(n)]
         self.components = n
         for idx in range(n):
             self.par[idx] = idx
             self.rank[idx] = 1
 
     def find(self, node):
-        root = self.par[node]
+        p = self.par[node]
 
-        while root != self.par[root]:
-            self.par[root] = self.par[self.par[root]]
-            root = self.par[root]
-
-        return root
+        while p != self.par[p]:
+            self.par[p] = self.par[self.par[p]]
+            p = self.par[p]
+        
+        return p
 
     def union(self, u, v):
         pu = self.find(u)
         pv = self.find(v)
-        
+
         if pu == pv:
             return False
-
+     
         self.components -= 1
-
         if self.rank[pu] > self.rank[pv]:
             self.par[pv] = pu
-        elif self.rank[pu] < self.rank[pv]:
+        elif  self.rank[pu] < self.rank[pv]:
             self.par[pu] = pv
         else:
-            self.par[pu] = pv
-            self.rank[pv] += 1
+            self.par[pv] = pu
+            self.rank[pu] += 1
         
         return True
-
+    
     def comps(self):
         return self.components
 
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         dsu = DSU(n)
-
-        for u,v in edges:
+        for u, v in edges:
             dsu.union(u,v)
-
+        
         return dsu.comps()
