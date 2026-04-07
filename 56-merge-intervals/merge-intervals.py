054901c542017f9cc98a36889f5359d7983ac261
Dhaval Patel <dhavaljp.95@gmail.com>
2026-04-07 10:48:08 -0400

    Time: 10 ms (29.63%) | Memory: 23.4 MB (5.66%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,17 +1,25 @@
+"""
+ip: [[1,3], [2,5], [4,7], [8,10]]
+ 
+init_start = 8
+init_end = 10
+interval =[8,10]
+res = [[1,7], [8,10]]
+"""
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals, key= lambda x:x[0])
-        start_time = intervals[0][0]
-        end_time = intervals[0][1]
+        intervals.sort()
+        initial_start = intervals[0][0]
+        initial_end = intervals[0][1]
         res = []
-        for start, end in intervals[1:]:
-            if start <= end_time:
-                if end_time <= end:
-                    end_time = end
-            else:
-                res.append([start_time, end_time])
-                start_time = start
-                end_time = end
+        for interval in intervals[1:]:
+            if interval[0] <= initial_end:
+                initial_end = max(initial_end, interval[1])
+            elif interval[0] > initial_end:
+                res.append([initial_start, initial_end])
+                initial_start = interval[0]
+                initial_end = interval[1]
         
-        res.append([start_time, end_time])
+        res.append([initial_start, initial_end])
+
         return res
