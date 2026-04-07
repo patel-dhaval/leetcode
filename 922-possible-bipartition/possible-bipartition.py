56b966bbbfbc17aacbda96d2b73dadbc5a1cef37
Dhaval Patel <dhavaljp.95@gmail.com>
2026-04-07 10:39:43 -0400

    Time: 22 ms (89.74%) | Memory: 22.8 MB (97.47%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.py
+++ 922-possible-bipartition/possible-bipartition.py
@@ -1,26 +1,32 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        adj_list = [[] for _ in range(n+1)]
-        queue = collections.deque()
+        graph = [[] for _ in range(n+1)]
 
         for src, dst in dislikes:
-            adj_list[src].append(dst)
-            adj_list[dst].append(src)
+            graph[src].append(dst)
+            graph[dst].append(src)
 
-        colors = [-1] * (n+1)
+        colors = [-1] * (n + 1)
+        queue = collections.deque()
 
-        for idx in range(1, n+1):
+        for idx in range(0, n):
+            if colors[idx] != -1:
+                continue
+
             if colors[idx] == -1:
                 queue.append(idx)
                 colors[idx] = 0
+                
             while queue:
-                curr = queue.popleft()
-                for neighbour in adj_list[curr]:
+                curr = queue.popleft()                
+                for neighbour in graph[curr]:
                     if colors[neighbour] == colors[curr]:
                         return False
                     if colors[neighbour] == -1:
                         colors[neighbour] = 1 - colors[curr]
                         queue.append(neighbour)
-        
-        
+                
+    
         return True
+
+        
