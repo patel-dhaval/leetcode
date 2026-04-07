b213e604fca46427b145787f7d988fbfe926477a
Dhaval Patel <dhavaljp.95@gmail.com>
2026-04-07 10:59:31 -0400

    Time: 6 ms (19.50%) | Memory: 19.2 MB (85.35%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,43 +1,49 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        neighbours = [[1,0], [0,1], [-1,0], [0,-1]]
         rows = len(grid)
         cols = len(grid[0])
         fresh_oranges = 0
-        rotten_oranges = 0
-        directions = [[1,0], [-1, 0], [0,1], [0,-1]]
+        rotting_oranges = 0
         queue = collections.deque()
-
+        visited = set()
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
                     fresh_oranges += 1
                 elif grid[r][c] == 2:
-                    rotten_oranges += 1
+                    rotting_oranges += 1
                     queue.append((r,c))
-        
+
         if fresh_oranges == 0:
             return 0
-
-        if rotten_oranges == 0:
+        
+        if rotting_oranges == 0:
             return -1
-        time = 0        
+        
+        time = 0
         while queue:
+            time += 1
             for _ in range(len(queue)):
-                r, c  =  queue.popleft()
-                for dr, dc in directions:
+                r,c = queue.popleft()
+                visited.add((r,c))
+                for dr, dc in neighbours:
                     nr = r + dr
                     nc = c + dc
-                    if min(nr, nc) < 0 or nr == rows or nc == cols or grid[nr][nc] == 0 or grid[nr][nc] == 2:
-                        continue
 
+                    if min(nr, nc) < 0 or nr == rows or nc == cols or grid[nr][nc] == 0 or grid[nr][nc] == 2 or (nr, nc ) in visited:
+                        continue
+                    
                     grid[nr][nc] = 2
                     fresh_oranges -= 1
                     queue.append((nr, nc))
+
                     if fresh_oranges == 0:
-                        return time + 1
-            time += 1
+                        return time
         
+
         return time if fresh_oranges == 0 else -1
 
 
+            
