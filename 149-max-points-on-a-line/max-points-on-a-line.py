c204d60ca9cb21f3a2baa34506821f4acd8769a8
Dhaval Patel <dhavaljp.95@gmail.com>
2026-04-08 18:06:50 -0400

    Time: 23 ms (84.72%) | Memory: 19.3 MB (92.55%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.py
+++ 149-max-points-on-a-line/max-points-on-a-line.py
@@ -1,29 +1,49 @@
+"""
+points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+max_points = 3
+p1 = [3,2]
+p2 = [4,1]
+dy = -1
+dx = 1
+slope_map = {-1: 3, 0.5: 1, 1:1, 2:1, inf: 1}
+
+points = [[1,1], [1,1], [1,1]]
+max_points = 3
+p1 = [1,1]
+p2 = [1,1]
+dy = 0
+dx = 0
+slope_map = {inf: 3}
+
+"""
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
         n = len(points)
-        if n <= 2: return n
+
+        if n <= 2:
+            return n
         
-        ans = 1
-        
-        for i in range(n):
+        max_points = 0
+
+        for idx in range(n):
+            p1 = points[idx]
+
             slope_map = collections.defaultdict(int)
-            p1 = points[i]
-            
-            for j in range(i + 1, n):
-                p2 = points[j]
-                
-                dx = p2[0] - p1[0]
+
+            for jdx in range(idx+1, n):
+                p2 = points[jdx]
+
                 dy = p2[1] - p1[1]
-                
-                # Handle Vertical Line (dx is 0)
+                dx = p2[0] - p1[0]
+
                 if dx == 0:
-                    slope = "inf"
+                    slope = float("inf")
                 else:
-                    # Reduce fraction using GCD
-                    # common = math.gcd(dy, dx)
                     slope = dy/dx
-                
+
                 slope_map[slope] += 1
-                ans = max(ans, slope_map[slope] + 1) # +1 includes the anchor point itself
-                
-        return ans
+            
+                max_points = max(max_points, slope_map[slope]+1)
+        
+
+        return max_points
